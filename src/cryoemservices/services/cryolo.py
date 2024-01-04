5c25a16beb1a285a26c250bb4651778237a098ac
stephen-riggs <stephen.riggs@diamond.ac.uk>
2024-01-04 10:37:42 +0000

    Add further SPA services from python-relion: cryolo, extract and particle selection

---
+++ src/cryoemservices/services/cryolo.py
@@ -0,0 +1,379 @@
+from __future__ import annotations
+
+import re
+import subprocess
+from pathlib import Path
+
+import numpy as np
+import workflows.recipe
+from gemmi import cif
+from pydantic import BaseModel, Field, ValidationError
+from workflows.services.common_service import CommonService
+
+from cryoemservices.util.spa_relion_service_options import RelionServiceOptions
+
+
+class CryoloParameters(BaseModel):
+    input_path: str = Field(..., min_length=1)
+    output_path: str = Field(..., min_length=1)
+    pixel_size: float
+    cryolo_config_file: str = "/dls_sw/apps/EM/crYOLO/phosaurus_models/config.json"
+    cryolo_model_weights: str = (
+        "/dls_sw/apps/EM/crYOLO/phosaurus_models/gmodel_phosnet_202005_N63_c17.h5"
+    )
+    cryolo_threshold: float = 0.3
+    retained_fraction: float = 1
+    cryolo_command: str = "cryolo_predict.py"
+    particle_diameter: float = None
+    mc_uuid: int
+    picker_uuid: int
+    relion_options: RelionServiceOptions
+    ctf_values: dict = {}
+
+
+class CrYOLO(CommonService):
+    """
+    A service that runs crYOLO particle picking
+    """
+
+    # Human readable service name
+    _service_name = "CrYOLO"
+
+    # Logger name
+    _logger_name = "cryoemservices.services.cryolo"
+
+    # Job name
+    job_type = "cryolo.autopick"
+
+    # Values to extract for ISPyB
+    number_of_particles: int
+
+    def initializing(self):
+        """Subscribe to a queue. Received messages must be acknowledged."""
+        self.log.info("crYOLO service starting")
+        workflows.recipe.wrap_subscribe(
+            self._transport,
+            "cryolo",
+            self.cryolo,
+            acknowledgement=True,
+            log_extender=self.extend_log,
+            allow_non_recipe_messages=True,
+        )
+
+    def parse_cryolo_output(self, cryolo_stdout: str):
+        """
+        Read the output logs of cryolo to determine
+        the number of particles that are picked
+        """
+        for line in cryolo_stdout.split("\n"):
+            if "particles in total has been found" in line:
+                line_split = line.split()
+                self.number_of_particles += int(line_split[0])
+
+            if line.startswith("Deleted"):
+                line_split = line.split()
+                self.number_of_particles -= int(line_split[1])
+
+    def cryolo(self, rw, header: dict, message: dict):
+        """
+        Main function which interprets received messages, runs cryolo
+        and sends messages to the ispyb and image services
+        """
+
+        class MockRW:
+            def dummy(self, *args, **kwargs):
+                pass
+
+        if not rw:
+            if (
+                not isinstance(message, dict)
+                or not message.get("parameters")
+                or not message.get("content")
+            ):
+                self.log.error("Rejected invalid simple message")
+                self._transport.nack(header)
+                return
+
+            # Create a wrapper-like object that can be passed to functions
+            # as if a recipe wrapper was present.
+            rw = MockRW()
+            rw.transport = self._transport
+            rw.recipe_step = {"parameters": message["parameters"]}
+            rw.environment = {"has_recipe_wrapper": False}
+            rw.set_default_channel = rw.dummy
+            rw.send = rw.dummy
+            message = message["content"]
+
+        # Reset number of particles
+        self.number_of_particles = 0
+
+        try:
+            if isinstance(message, dict):
+                cryolo_params = CryoloParameters(
+                    **{**rw.recipe_step.get("parameters", {}), **message}
+                )
+            else:
+                cryolo_params = CryoloParameters(
+                    **{**rw.recipe_step.get("parameters", {})}
+                )
+        except (ValidationError, TypeError) as e:
+            self.log.warning(
+                f"crYOLO parameter validation failed for message: {message} "
+                f"and recipe parameters: {rw.recipe_step.get('parameters', {})} "
+                f"with exception: {e}"
+            )
+            rw.transport.nack(header)
+            return
+
+        # CrYOLO requires running in the project directory or job directory
+        job_dir = Path(re.search(".+/job[0-9]{3}/", cryolo_params.output_path)[0])
+        job_dir.mkdir(parents=True, exist_ok=True)
+
+        # Construct a command to run cryolo with the given parameters
+        command = cryolo_params.cryolo_command.split()
+        command.extend((["--conf", cryolo_params.cryolo_config_file]))
+        command.extend((["-o", str(job_dir)]))
+        command.extend((["--otf"]))
+
+        cryolo_flags = {
+            "cryolo_model_weights": "--weights",
+            "input_path": "-i",
+            "cryolo_threshold": "--threshold",
+            "cryolo_gpus": "--gpu",
+        }
+
+        for k, v in cryolo_params.dict().items():
+            if v and (k in cryolo_flags):
+                command.extend((cryolo_flags[k], str(v)))
+
+        self.log.info(
+            f"Input: {cryolo_params.input_path} "
+            + f"Output: {cryolo_params.output_path}"
+        )
+
+        # Run cryolo
+        result = subprocess.run(command, cwd=job_dir, capture_output=True)
+
+        # Remove cryosparc file to minimise crashes
+        (job_dir / "CRYOSPARC/cryosparc.star").unlink(missing_ok=True)
+
+        # Read in the stdout from cryolo
+        self.parse_cryolo_output(result.stdout.decode("utf8", "replace"))
+
+        # Read in the cbox file for particle selection and finding sizes
+        try:
+            cbox_file = cif.read_file(
+                str(
+                    job_dir
+                    / f"CBOX/{Path(cryolo_params.output_path).with_suffix('.cbox').name}"
+                )
+            )
+            cbox_block = cbox_file.find_block("cryolo")
+            cbox_sizes = np.append(
+                np.array(cbox_block.find_loop("_EstWidth"), dtype=float),
+                np.array(cbox_block.find_loop("_EstHeight"), dtype=float),
+            )
+            cbox_confidence = np.append(
+                np.array(cbox_block.find_loop("_Confidence"), dtype=float),
+                np.array(cbox_block.find_loop("_Confidence"), dtype=float),
+            )
+
+            # Select only a fraction of particles based on confidence if requested
+            if (
+                cryolo_params.retained_fraction < 1
+                and cryolo_params.retained_fraction > 0
+            ):
+                particles_confidence = np.array(
+                    cbox_block.find_loop("_Confidence"), dtype=float
+                )
+                cryolo_threshold = np.quantile(
+                    particles_confidence, 1 - cryolo_params.retained_fraction
+                )
+
+                self.log.info(
+                    f"Selecting particles with confidence above {cryolo_threshold}"
+                )
+
+                particles_x_all = np.array(
+                    cbox_block.find_loop("_CoordinateX"), dtype=float
+                )
+                particles_y_all = np.array(
+                    cbox_block.find_loop("_CoordinateY"), dtype=float
+                )
+                box_size_x_all = np.array(cbox_block.find_loop("_Width"), dtype=float)
+                box_size_y_all = np.array(cbox_block.find_loop("_Height"), dtype=float)
+
+                thresholded_x = (particles_x_all + box_size_x_all / 2)[
+                    particles_confidence > cryolo_threshold
+                ]
+                thresholded_y = (particles_y_all + box_size_y_all / 2)[
+                    particles_confidence > cryolo_threshold
+                ]
+
+                # Rewrite the star file with only the selected particles
+                with open(job_dir / cryolo_params.output_path, "w") as particles_star:
+                    particles_star.write(
+                        "\ndata_\n\nloop_\n_rlnCoordinateX #1\n_rlnCoordinateY #2\n"
+                    )
+                    for particle in range(len(thresholded_x)):
+                        particles_star.write(
+                            f"{thresholded_x[particle]} {thresholded_y[particle]}\n"
+                        )
+            else:
+                cryolo_threshold = cryolo_params.cryolo_threshold
+            cryolo_params.relion_options.cryolo_threshold = cryolo_threshold
+
+            # Get the diameters of the particles for murfey
+            cryolo_particle_sizes = cbox_sizes[cbox_confidence > cryolo_threshold]
+        except (FileNotFoundError, OSError, AttributeError):
+            cryolo_particle_sizes = []
+
+        # Register the cryolo job with the node creator
+        self.log.info(f"Sending {self.job_type} to node creator")
+        node_creator_parameters = {
+            "job_type": self.job_type,
+            "input_file": cryolo_params.input_path,
+            "output_file": cryolo_params.output_path,
+            "relion_options": dict(cryolo_params.relion_options),
+            "command": " ".join(command),
+            "stdout": result.stdout.decode("utf8", "replace"),
+            "stderr": result.stderr.decode("utf8", "replace"),
+        }
+        if (
+            result.returncode
+            and result.stderr.decode("utf8", "replace").split("\n")[-1]
+            == "IndexError: list index out of range"
+        ):
+            # If Cryolo failed because there are no picks then consider it a success
+            result.returncode = 0
+
+        if result.returncode:
+            node_creator_parameters["success"] = False
+        else:
+            node_creator_parameters["success"] = True
+        if isinstance(rw, MockRW):
+            rw.transport.send(
+                destination="node_creator",
+                message={"parameters": node_creator_parameters, "content": "dummy"},
+            )
+        else:
+            rw.send_to("node_creator", node_creator_parameters)
+
+        # End here if the command failed
+        if result.returncode:
+            self.log.error(
+                f"crYOLO failed with exitcode {result.returncode}:\n"
+                + result.stderr.decode("utf8", "replace")
+            )
+            rw.transport.nack(header)
+            return
+
+        # Forward results to ISPyB
+        ispyb_parameters = {
+            "ispyb_command": "buffer",
+            "buffer_lookup": {"motion_correction_id": cryolo_params.mc_uuid},
+            "buffer_command": {"ispyb_command": "insert_particle_picker"},
+            "buffer_store": cryolo_params.picker_uuid,
+            "particle_picking_template": cryolo_params.cryolo_model_weights,
+            "number_of_particles": self.number_of_particles,
+            "summary_image_full_path": str(
+                Path(cryolo_params.output_path).with_suffix(".jpeg")
+            ),
+        }
+        if cryolo_params.particle_diameter:
+            ispyb_parameters["particle_diameter"] = cryolo_params.particle_diameter
+        self.log.info(f"Sending to ispyb {ispyb_parameters}")
+        if isinstance(rw, MockRW):
+            rw.transport.send(
+                destination="ispyb_connector",
+                message={
+                    "parameters": ispyb_parameters,
+                    "content": {"dummy": "dummy"},
+                },
+            )
+        else:
+            rw.send_to("ispyb_connector", ispyb_parameters)
+
+        # Extract results for images service
+        try:
+            with open(cryolo_params.output_path, "r") as coords_file:
+                coords = [line.split() for line in coords_file][6:]
+        except FileNotFoundError:
+            coords = []
+
+        # Forward results to images service
+        self.log.info("Sending to images service")
+        if isinstance(rw, MockRW):
+            rw.transport.send(
+                destination="images",
+                message={
+                    "image_command": "picked_particles",
+                    "file": cryolo_params.input_path,
+                    "coordinates": coords,
+                    "pixel_size": cryolo_params.pixel_size,
+                    "diameter": cryolo_params.pixel_size * 160,
+                    "outfile": str(
+                        Path(cryolo_params.output_path).with_suffix(".jpeg")
+                    ),
+                },
+            )
+        else:
+            rw.send_to(
+                "images",
+                {
+                    "image_command": "picked_particles",
+                    "file": cryolo_params.input_path,
+                    "coordinates": coords,
+                    "pixel_size": cryolo_params.pixel_size,
+                    "diameter": cryolo_params.pixel_size * 160,
+                    "outfile": str(
+                        Path(cryolo_params.output_path).with_suffix(".jpeg")
+                    ),
+                },
+            )
+
+        # Gather results needed for particle extraction
+        extraction_params = {
+            "ctf_values": cryolo_params.ctf_values,
+            "micrographs_file": cryolo_params.input_path,
+            "coord_list_file": cryolo_params.output_path,
+        }
+        job_number = int(re.search("/job[0-9]{3}/", cryolo_params.output_path)[0][4:7])
+        extraction_params["extract_file"] = str(
+            Path(
+                re.sub(
+                    "MotionCorr/job002/.+",
+                    f"Extract/job{job_number + 1:03}/Movies/",
+                    cryolo_params.input_path,
+                )
+            )
+            / (Path(cryolo_params.input_path).stem + "_extract.star")
+        )
+
+        # Forward results to murfey
+        self.log.info("Sending to Murfey for particle extraction")
+        if isinstance(rw, MockRW):
+            rw.transport.send(
+                destination="murfey_feedback",
+                message={
+                    "register": "picked_particles",
+                    "motion_correction_id": cryolo_params.mc_uuid,
+                    "micrograph": cryolo_params.input_path,
+                    "particle_diameters": list(cryolo_particle_sizes),
+                    "extraction_parameters": extraction_params,
+                },
+            )
+        else:
+            rw.send_to(
+                "murfey_feedback",
+                {
+                    "register": "picked_particles",
+                    "motion_correction_id": cryolo_params.mc_uuid,
+                    "micrograph": cryolo_params.input_path,
+                    "particle_diameters": list(cryolo_particle_sizes),
+                    "extraction_parameters": extraction_params,
+                },
+            )
+
+        self.log.info(f"Done {self.job_type} for {cryolo_params.input_path}.")
+        rw.transport.ack(header)
---
+++ tests/services/test_cryolo_service.py
@@ -0,0 +1,202 @@
+from __future__ import annotations
+
+import sys
+from unittest import mock
+
+import pytest
+import zocalo.configuration
+from workflows.transport.offline_transport import OfflineTransport
+
+from cryoemservices.services import cryolo
+from cryoemservices.util.spa_relion_service_options import RelionServiceOptions
+
+
+@pytest.fixture
+def mock_zocalo_configuration(tmp_path):
+    mock_zc = mock.MagicMock(zocalo.configuration.Configuration)
+    mock_zc.storage = {
+        "zocalo.recipe_directory": tmp_path,
+    }
+    return mock_zc
+
+
+@pytest.fixture
+def mock_environment(mock_zocalo_configuration):
+    return {"config": mock_zocalo_configuration}
+
+
+@pytest.fixture
+def offline_transport(mocker):
+    transport = OfflineTransport()
+    mocker.spy(transport, "send")
+    return transport
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
+@mock.patch("cryoemservices.services.cryolo.subprocess.run")
+def test_cryolo_service(mock_subprocess, mock_environment, offline_transport, tmp_path):
+    """
+    Send a test message to CrYOLO
+    This should call the mock subprocess then send messages on to the
+    node_creator, murfey_feedback, ispyb_connector and images services
+    """
+    mock_subprocess().returncode = 0
+    mock_subprocess().stdout = "stdout".encode("ascii")
+    mock_subprocess().stderr = "stderr".encode("ascii")
+
+    header = {
+        "message-id": mock.sentinel,
+        "subscription": mock.sentinel,
+    }
+
+    output_path = tmp_path / "AutoPick/job007/STAR/sample.star"
+    cbox_path = tmp_path / "AutoPick/job007/CBOX/sample.cbox"
+    cryolo_test_message = {
+        "parameters": {
+            "boxsize": 256,
+            "pixel_size": 0.1,
+            "input_path": "MotionCorr/job002/sample.mrc",
+            "output_path": str(output_path),
+            "cryolo_config_file": str(tmp_path) + "/config.json",
+            "cryolo_model_weights": "sample_weights",
+            "cryolo_threshold": 0.15,
+            "retained_fraction": 0.5,
+            "mc_uuid": 0,
+            "picker_uuid": 0,
+            "particle_diameter": 1.1,
+            "ctf_values": {"dummy": "dummy"},
+            "cryolo_command": "cryolo_predict.py",
+            "relion_options": {"batch_size": 20000, "downscale": True},
+        },
+        "content": "dummy",
+    }
+    output_relion_options = dict(RelionServiceOptions())
+    output_relion_options["cryolo_threshold"] = 0.55
+    output_relion_options.update(cryolo_test_message["parameters"]["relion_options"])
+
+    # Write a dummy config file expected by cryolo
+    with open(tmp_path / "config.json", "w") as f:
+        f.write('{\n"model": {\n"anchors": [160, 160]\n}\n}')
+
+    # Write star co-ordinate file in the format cryolo will output
+    output_path.parent.mkdir(parents=True)
+    with open(output_path, "w") as f:
+        f.write("data_\n\nloop_\n\n_rlnCoordinateX\n_rlnCoordinateY\n 1.1 2.2")
+    cbox_path.parent.mkdir(parents=True)
+    with open(cbox_path, "w") as f:
+        f.write(
+            "data_cryolo\n\nloop_\n\n_EstWidth\n_EstHeight\n_Confidence\n"
+            "_CoordinateX\n_CoordinateY\n_Width\n_Height\n"
+            "100 200 0.6 0.1 0.2 2 4\n100 200 0.5 0.3 0.4 6 8"
+        )
+
+    # Make the cryolo temporary dirs
+    (tmp_path / "logs").mkdir()
+    (tmp_path / "filtered").mkdir()
+
+    # Set up the mock service and send the message to it
+    service = cryolo.CrYOLO(environment=mock_environment)
+    service.transport = offline_transport
+    service.start()
+    service.cryolo(None, header=header, message=cryolo_test_message)
+
+    assert mock_subprocess.call_count == 4
+    mock_subprocess.assert_called_with(
+        [
+            "cryolo_predict.py",
+            "--conf",
+            str(tmp_path / "config.json"),
+            "-o",
+            str(output_path.parent.parent),
+            "--otf",
+            "-i",
+            "MotionCorr/job002/sample.mrc",
+            "--weights",
+            "sample_weights",
+            "--threshold",
+            "0.15",
+        ],
+        cwd=tmp_path / "AutoPick/job007",
+        capture_output=True,
+    )
+
+    # Check that the correct messages were sent
+    extraction_params = {
+        "ctf_values": cryolo_test_message["parameters"]["ctf_values"],
+        "micrographs_file": cryolo_test_message["parameters"]["input_path"],
+        "coord_list_file": cryolo_test_message["parameters"]["output_path"],
+        "extract_file": "Extract/job008/Movies/sample_extract.star",
+    }
+    offline_transport.send.assert_any_call(
+        destination="ispyb_connector",
+        message={
+            "parameters": {
+                "particle_picking_template": "sample_weights",
+                "number_of_particles": 0,
+                "particle_diameter": 1.1,
+                "summary_image_full_path": str(output_path.with_suffix(".jpeg")),
+                "ispyb_command": "buffer",
+                "buffer_lookup": {"motion_correction_id": 0},
+                "buffer_command": {"ispyb_command": "insert_particle_picker"},
+                "buffer_store": 0,
+            },
+            "content": {"dummy": "dummy"},
+        },
+    )
+    offline_transport.send.assert_any_call(
+        destination="images",
+        message={
+            "image_command": "picked_particles",
+            "file": cryolo_test_message["parameters"]["input_path"],
+            "coordinates": [["1.1", "2.2"]],
+            "pixel_size": 0.1,
+            "diameter": 16.0,
+            "outfile": str(output_path.with_suffix(".jpeg")),
+        },
+    )
+    offline_transport.send.assert_any_call(
+        destination="murfey_feedback",
+        message={
+            "register": "picked_particles",
+            "motion_correction_id": cryolo_test_message["parameters"]["mc_uuid"],
+            "micrograph": cryolo_test_message["parameters"]["input_path"],
+            "particle_diameters": [100.0, 200.0],
+            "extraction_parameters": extraction_params,
+        },
+    )
+    offline_transport.send.assert_any_call(
+        destination="node_creator",
+        message={
+            "parameters": {
+                "job_type": "cryolo.autopick",
+                "input_file": cryolo_test_message["parameters"]["input_path"],
+                "output_file": str(output_path),
+                "relion_options": output_relion_options,
+                "command": (
+                    f"cryolo_predict.py --conf {tmp_path}/config.json "
+                    f"-o {tmp_path}/AutoPick/job007 --otf "
+                    f"-i MotionCorr/job002/sample.mrc "
+                    f"--weights sample_weights --threshold 0.15"
+                ),
+                "stdout": "stdout",
+                "stderr": "stderr",
+                "success": True,
+            },
+            "content": "dummy",
+        },
+    )
+
+
+def test_parse_cryolo_output(mock_environment, offline_transport):
+    """
+    Send test lines to the output parser
+    to check the number of particles is being read in
+    """
+    service = cryolo.CrYOLO(environment=mock_environment)
+    service.transport = offline_transport
+    service.start()
+
+    service.number_of_particles = 0
+    cryolo.CrYOLO.parse_cryolo_output(service, "30 particles in total has been found")
+    cryolo.CrYOLO.parse_cryolo_output(service, "Deleted 10 particles")
+    assert service.number_of_particles == 20
