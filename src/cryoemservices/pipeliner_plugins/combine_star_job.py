78d40b454d5df53e34abe6ff74781e4036320ebc
stephen-riggs <stephen.riggs@diamond.ac.uk>
2024-01-08 10:47:07 +0000

    Add display object method to star combination job

--- src/cryoemservices/pipeliner_plugins/combine_star_job.py
+++ src/cryoemservices/pipeliner_plugins/combine_star_job.py
@@ -3,9 +3,15 @@ from __future__ import annotations
 from pathlib import Path
 
 from pipeliner.data_structure import SELECT_DIR
+from pipeliner.display_tools import mini_montage_from_starfile
 from pipeliner.job_options import BooleanJobOption, IntJobOption, StringJobOption
 from pipeliner.nodes import NODE_PARTICLESDATA, Node
-from pipeliner.pipeliner_job import ExternalProgram, PipelinerCommand, PipelinerJob
+from pipeliner.pipeliner_job import ExternalProgram, PipelinerJob
+
+try:
+    from pipeliner.pipeliner_job import PipelinerCommand
+except ImportError:
+    PipelinerCommand = None
 
 COMBINE_STAR_NAME = "combine_star_files"
 
@@ -85,12 +91,14 @@ class ProcessStarFiles(PipelinerJob):
 
         # Add files as input nodes, as long as they are not also the output node
         for particle_file in file_list:
-            if particle_file != self.output_dir + "particles_all.star":
+            if particle_file != str(Path(self.output_dir) / "particles_all.star"):
                 self.input_nodes.append(Node(particle_file, NODE_PARTICLESDATA))
         self.output_nodes.append(
-            Node(self.output_dir + "particles_all.star", NODE_PARTICLESDATA)
+            Node(str(Path(self.output_dir) / "particles_all.star"), NODE_PARTICLESDATA)
         )
 
+        if PipelinerCommand is None:
+            return [command]
         pipeliner_commands = [PipelinerCommand([command], relion_control=False)]
         return pipeliner_commands
 
@@ -102,3 +110,46 @@ class ProcessStarFiles(PipelinerJob):
         output_files = Path(self.output_dir).glob("particles_split*.star")
         for split in output_files:
             self.output_nodes.append(Node(str(split), NODE_PARTICLESDATA))
+
+    def create_results_display(self):
+        with open(
+            Path(self.output_dir) / "class_averages.star", "r"
+        ) as all_classes, open(
+            Path(self.output_dir) / ".class_display_tmp.star", "w"
+        ) as display_classes:
+            for line in range(200):
+                class_line = all_classes.readline()
+                if not class_line:
+                    break
+                display_classes.write(class_line)
+        with open(
+            Path(self.output_dir) / "particles_all.star", "r"
+        ) as all_particles, open(
+            Path(self.output_dir) / ".particles_display_tmp.star", "w"
+        ) as display_particles:
+            for line in range(200):
+                particles_line = all_particles.readline()
+                if not particles_line:
+                    break
+                display_particles.write(particles_line)
+        output_dobs = [
+            mini_montage_from_starfile(
+                starfile=str(Path(self.output_dir) / ".class_display_tmp.star"),
+                block="",
+                column="_rlnReferenceImage",
+                outputdir=self.output_dir,
+                nimg=100,
+                title="Selected 2D classes",
+            ),
+            mini_montage_from_starfile(
+                starfile=str(Path(self.output_dir) / ".particles_display_tmp.star"),
+                block="particles",
+                column="_rlnImageName",
+                outputdir=self.output_dir,
+                nimg=20,
+                title="Examples of selected particles",
+            ),
+        ]
+        (Path(self.output_dir) / ".class_display_tmp.star").unlink()
+        (Path(self.output_dir) / ".particles_display_tmp.star").unlink()
+        return output_dobs
