72890b53c130dc0f2dcaa810294e1736f9c248e2
yxd92326 <stephen.riggs@diamond.ac.uk>
2024-06-10 12:11:35 +0100

    Move motioncor2 slurm tests

--- tests/services/test_motioncorr_service.py
+++ tests/services/test_motioncorr_service.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import sys
 from pathlib import Path
 from unittest import mock
@@ -328,6 +329,155 @@ def test_motioncor2_service_spa(
                 "command": " ".join(mc_command),
                 "stdout": "stdout",
                 "stderr": "stderr",
+                "results": {
+                    "total_motion": total_motion,
+                    "early_motion": early_motion,
+                    "late_motion": late_motion,
+                },
+            },
+            "content": "dummy",
+        },
+    )
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
+@mock.patch("cryoemservices.util.slurm_submission.subprocess.run")
+def test_motioncor2_slurm_service_spa(
+    mock_subprocess, mock_environment, offline_transport, tmp_path
+):
+    """
+    Send a test message to MotionCorr for SPA using MotionCor2 via slurm.
+    """
+    mock_subprocess().returncode = 0
+    mock_subprocess().stdout = (
+        '{"job_id": "1", "jobs": [{"job_state": ["COMPLETED"]}]}'.encode("ascii")
+    )
+    mock_subprocess().stderr = "stderr".encode("ascii")
+
+    movie = Path(f"{tmp_path}/Movies/sample.tiff")
+    movie.parent.mkdir(parents=True)
+    movie.touch()
+
+    header = {
+        "message-id": mock.sentinel,
+        "subscription": mock.sentinel,
+    }
+    motioncorr_test_message = {
+        "parameters": {
+            "movie": str(movie),
+            "mrc_out": f"{tmp_path}/MotionCorr/job002/Movies/sample.mrc",
+            "experiment_type": "spa",
+            "pixel_size": 0.1,
+            "dose_per_frame": 1,
+            "use_motioncor2": True,
+            "submit_to_slurm": True,
+            "patch_sizes": {"x": 5, "y": 5},
+            "movie_id": 1,
+            "mc_uuid": 0,
+            "picker_uuid": 0,
+            "relion_options": {},
+        },
+        "content": "dummy",
+    }
+    output_relion_options = dict(RelionServiceOptions())
+    output_relion_options["pixel_size"] = motioncorr_test_message["parameters"][
+        "pixel_size"
+    ]
+    output_relion_options["dose_per_frame"] = motioncorr_test_message["parameters"][
+        "dose_per_frame"
+    ]
+    output_relion_options["eer_grouping"] = 0
+
+    # Set up the mock service
+    service = motioncorr.MotionCorr(environment=mock_environment)
+    service.transport = offline_transport
+    service.start()
+
+    # Work out the expected shifts
+    service.x_shift_list = [-3.0, 3.0]
+    service.y_shift_list = [4.0, -4.0]
+    service.each_total_motion = [5.0, 5.0]
+    total_motion = 10.0
+    early_motion = 10.0
+    late_motion = 0.0
+
+    # Construct the file which contains rest api submission information
+    os.environ["MOTIONCOR2_SIF"] = "MotionCor2_SIF"
+    os.environ["SLURM_RESTAPI_CONFIG"] = str(tmp_path / "restapi.txt")
+    with open(tmp_path / "restapi.txt", "w") as restapi_config:
+        restapi_config.write(
+            "user: user\n"
+            "user_home: /home\n"
+            f"user_token: {tmp_path}/token.txt\n"
+            "required_directories: [directory1, directory2]\n"
+            "partition: partition\n"
+            "partition_preference: preference\n"
+            "cluster: cluster\n"
+            "url: /url/of/slurm/restapi\n"
+            "api_version: v0.0.40\n"
+        )
+    with open(tmp_path / "token.txt", "w") as token:
+        token.write("token_key")
+
+    # Touch the expected output files
+    (tmp_path / "MotionCorr/job002/Movies").mkdir(parents=True)
+    (tmp_path / "MotionCorr/job002/Movies/sample.mrc.out").touch()
+    (tmp_path / "MotionCorr/job002/Movies/sample.mrc.err").touch()
+
+    # Send a message to the service
+    service.motion_correction(None, header=header, message=motioncorr_test_message)
+
+    # Get the expected motion correction command
+    mc_command = [
+        "MotionCor2",
+        "-InTiff",
+        str(movie),
+        "-OutMrc",
+        motioncorr_test_message["parameters"]["mrc_out"],
+        "-PixSize",
+        str(motioncorr_test_message["parameters"]["pixel_size"]),
+        "-FmDose",
+        "1.0",
+        "-Patch",
+        "5 5",
+        "-Gpu",
+        "0",
+        "-FmRef",
+        "0",
+    ]
+
+    # Check the slurm commands were run
+    slurm_submit_command = (
+        f'curl -H "X-SLURM-USER-NAME:user" -H "X-SLURM-USER-TOKEN:token_key" '
+        '-H "Content-Type: application/json" -X POST '
+        "/url/of/slurm/restapi/slurm/v0.0.40/job/submit "
+        f"-d @{tmp_path}/MotionCorr/job002/Movies/sample.mrc.json"
+    )
+    slurm_status_command = (
+        'curl -H "X-SLURM-USER-NAME:user" -H "X-SLURM-USER-TOKEN:token_key" '
+        '-H "Content-Type: application/json" -X GET '
+        "/url/of/slurm/restapi/slurm/v0.0.40/job/1"
+    )
+    assert mock_subprocess.call_count == 5
+    mock_subprocess.assert_any_call(
+        slurm_submit_command, capture_output=True, shell=True
+    )
+    mock_subprocess.assert_any_call(
+        slurm_status_command, capture_output=True, shell=True
+    )
+
+    # Just check the node creator send to make sure all ran correctly
+    offline_transport.send.assert_any_call(
+        destination="node_creator",
+        message={
+            "parameters": {
+                "job_type": "relion.motioncorr.motioncor2",
+                "input_file": f"{tmp_path}/Import/job001/Movies/sample.tiff",
+                "output_file": motioncorr_test_message["parameters"]["mrc_out"],
+                "relion_options": output_relion_options,
+                "command": " ".join(mc_command),
+                "stdout": "",
+                "stderr": "",
                 "results": {
                     "total_motion": total_motion,
                     "early_motion": early_motion,
@@ -805,6 +955,114 @@ def test_motioncor2_service_tomo(
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
+@mock.patch("cryoemservices.util.slurm_submission.subprocess.run")
+def test_motioncor2_slurm_service_tomo(
+    mock_subprocess, mock_environment, offline_transport, tmp_path
+):
+    """
+    Send a test message to MotionCorr for tomography using MotionCor2 via slurm
+    """
+    mock_subprocess().returncode = 0
+    mock_subprocess().stdout = (
+        '{"job_id": "1", "jobs": [{"job_state": ["COMPLETED"]}]}'.encode("ascii")
+    )
+    mock_subprocess().stderr = "stderr".encode("ascii")
+
+    movie = Path(f"{tmp_path}/Movies/sample.tiff")
+    movie.parent.mkdir(parents=True)
+    movie.touch()
+
+    header = {
+        "message-id": mock.sentinel,
+        "subscription": mock.sentinel,
+    }
+    motioncorr_test_message = {
+        "parameters": {
+            "movie": str(movie),
+            "mrc_out": f"{tmp_path}/MotionCorr/Movies/sample_motion_corrected.mrc",
+            "experiment_type": "tomography",
+            "pixel_size": 0.1,
+            "dose_per_frame": 1,
+            "use_motioncor2": True,
+            "submit_to_slurm": True,
+            "patch_sizes": {"x": 5, "y": 5},
+            "movie_id": 1,
+            "mc_uuid": 0,
+            "picker_uuid": 0,
+            "relion_options": {},
+        },
+        "content": "dummy",
+    }
+
+    # Set up the mock service
+    service = motioncorr.MotionCorr(environment=mock_environment)
+    service.transport = offline_transport
+    service.start()
+
+    # Work out the expected shifts
+    service.x_shift_list = [-3.0, 3.0]
+    service.y_shift_list = [4.0, -4.0]
+    service.each_total_motion = [5.0, 5.0]
+
+    # Construct the file which contains rest api submission information
+    os.environ["MOTIONCOR2_SIF"] = "MotionCor2_SIF"
+    os.environ["SLURM_RESTAPI_CONFIG"] = str(tmp_path / "restapi.txt")
+    with open(tmp_path / "restapi.txt", "w") as restapi_config:
+        restapi_config.write(
+            "user: user\n"
+            "user_home: /home\n"
+            f"user_token: {tmp_path}/token.txt\n"
+            "required_directories: [directory1, directory2]\n"
+            "partition: partition\n"
+            "partition_preference: preference\n"
+            "cluster: cluster\n"
+            "url: /url/of/slurm/restapi\n"
+            "api_version: v0.0.40\n"
+        )
+    with open(tmp_path / "token.txt", "w") as token:
+        token.write("token_key")
+
+    # Touch the expected output files
+    (tmp_path / "MotionCorr/Movies").mkdir(parents=True)
+    (tmp_path / "MotionCorr/Movies/sample_motion_corrected.mrc.out").touch()
+    (tmp_path / "MotionCorr/Movies/sample_motion_corrected.mrc.err").touch()
+
+    # Send a message to the service
+    service.motion_correction(None, header=header, message=motioncorr_test_message)
+
+    # Check the slurm commands were run
+    slurm_submit_command = (
+        f'curl -H "X-SLURM-USER-NAME:user" -H "X-SLURM-USER-TOKEN:token_key" '
+        '-H "Content-Type: application/json" -X POST '
+        "/url/of/slurm/restapi/slurm/v0.0.40/job/submit "
+        f"-d @{tmp_path}/MotionCorr/Movies/sample_motion_corrected.mrc.json"
+    )
+    slurm_status_command = (
+        'curl -H "X-SLURM-USER-NAME:user" -H "X-SLURM-USER-TOKEN:token_key" '
+        '-H "Content-Type: application/json" -X GET '
+        "/url/of/slurm/restapi/slurm/v0.0.40/job/1"
+    )
+    assert mock_subprocess.call_count == 5
+    mock_subprocess.assert_any_call(
+        slurm_submit_command, capture_output=True, shell=True
+    )
+    mock_subprocess.assert_any_call(
+        slurm_status_command, capture_output=True, shell=True
+    )
+
+    # Just check the murfey send to make sure all ran through
+    offline_transport.send.assert_any_call(
+        destination="murfey_feedback",
+        message={
+            "register": "motion_corrected",
+            "movie": str(movie),
+            "mrc_out": motioncorr_test_message["parameters"]["mrc_out"],
+            "movie_id": motioncorr_test_message["parameters"]["movie_id"],
+        },
+    )
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 @mock.patch("cryoemservices.services.motioncorr.subprocess.run")
 def test_motioncor_relion_service_tomo(
     mock_subprocess, mock_environment, offline_transport, tmp_path
@@ -1015,6 +1273,26 @@ def test_parse_motioncor2_output(mock_environment, offline_transport):
     motioncorr.MotionCorr.parse_mc2_stdout(
         service, "Frame   x Shift   y Shift\n1    -3.0      4.0\n2    3.0      -4.0"
     )
+    assert service.x_shift_list == [-3.0, 3.0]
+    assert service.y_shift_list == [4.0, -4.0]
+
+
+def test_parse_motioncor2_slurm_output(mock_environment, offline_transport, tmp_path):
+    """
+    Send test lines to the output parser
+    to check the shift values are being read in
+    """
+    service = motioncorr.MotionCorr(environment=mock_environment)
+    service.transport = offline_transport
+    service.start()
+
+    with open(tmp_path / "mc_output.txt", "w") as mc_output:
+        mc_output.write(
+            "...... Frame (  1) shift:    -3.0      4.0\n"
+            "...... Frame (  2) shift:    3.0      -4.0\n"
+        )
+
+    motioncorr.MotionCorr.parse_mc2_slurm_output(service, tmp_path / "mc_output.txt")
     assert service.x_shift_list == [-3.0, 3.0]
     assert service.y_shift_list == [4.0, -4.0]
 
