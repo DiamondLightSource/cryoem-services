f00dd91971b87465dda6ee84acacefa422e4cfe9
yxd92326 <stephen.riggs@diamond.ac.uk>
2024-08-28 09:27:19 +0100

    More MockRW removal

--- src/cryoemservices/services/bfactor_setup.py
+++ src/cryoemservices/services/bfactor_setup.py
@@ -8,6 +8,7 @@ import workflows.recipe
 from pydantic import BaseModel, Field, ValidationError
 from workflows.services.common_service import CommonService
 
+from cryoemservices.util.models import MockRW
 from cryoemservices.util.relion_service_options import (
     RelionServiceOptions,
     update_relion_options,
@@ -52,10 +53,7 @@ class BFactor(CommonService):
         )
 
     def bfactor_setup(self, rw, header: dict, message: dict):
-        class MockRW:
-            def dummy(self, *args, **kwargs):
-                pass
-
+        """Main function which interprets and processes received messages"""
         if not rw:
             print(
                 "Incoming message is not a recipe message. Simple messages can be valid"
@@ -72,12 +70,8 @@ class BFactor(CommonService):
 
             # Create a wrapper-like object that can be passed to functions
             # as if a recipe wrapper was present.
-            rw = MockRW()
-            rw.transport = self._transport
+            rw = MockRW(self._transport)
             rw.recipe_step = {"parameters": message["parameters"]}
-            rw.environment = {"has_recipe_wrapper": False}
-            rw.set_default_channel = rw.dummy
-            rw.send = rw.dummy
             message = message["content"]
 
         try:
