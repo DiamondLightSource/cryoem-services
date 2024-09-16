f01752104cc193eedc7f2acaec93e20dcfbe521e
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-09-16 06:02:37 +0100

    Added CLI script for processing CLEM image stacks

---
+++ src/cryoemservices/cli/clem_process_stacks.py
@@ -0,0 +1,11 @@
+"""
+A cryoEM service command line interface to facilitate the processing of image stacks
+from a single CLEM dataset, generating colorised composite images or image stacks for
+use in the subsequent stage of the CLEM workflow.
+"""
+
+from __future__ import annotations
+
+
+def run():
+    return True
