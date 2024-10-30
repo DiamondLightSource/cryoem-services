8352f863c430cb439e2c6968fb923dd6fc8ff652
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-10-30 11:53:12 +0000

    Added type hints and print function results to log output

--- src/cryoemservices/cli/clem_lif_to_stack.py
+++ src/cryoemservices/cli/clem_lif_to_stack.py
@@ -7,6 +7,7 @@ from cryoemservices.wrappers.clem_process_raw_lifs import convert_lif_to_stack
 
 
 def run():
+    # Create argument parser and needed arguments
     parser = argparse.ArgumentParser(
         description="Convert LIF files into TIFF image stacks"
     )
@@ -29,11 +30,21 @@ def run():
         default=1,
         help="Number of processes to run",
     )
-
     args = parser.parse_args()
 
-    convert_lif_to_stack(
-        file=Path(args.lif_path),
-        root_folder=args.root_dir,
-        number_of_processes=args.num_procs,
+    # Load args
+    lif_path = Path(args.lif_path)
+    root_folder: str = args.root_dir
+    num_procs: int = args.num_procs
+
+    # Run function
+    results = convert_lif_to_stack(
+        file=lif_path,
+        root_folder=root_folder,
+        number_of_processes=num_procs,
     )
+
+    # Print results in output log
+    if results is not None:
+        for result in results:
+            print(result)
