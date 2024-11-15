9308201b697003c3483ef7c1743e1fe3bcfaeaed
yxd92326 <stephen.riggs@diamond.ac.uk>
2024-11-15 12:40:30 +0000

    Tests for starting services and wrappers

--- src/cryoemservices/util/config.py
+++ src/cryoemservices/util/config.py
@@ -20,7 +20,7 @@ class ServiceConfig(BaseModel):
     @model_validator(mode="before")
     @classmethod
     def check_port_present_if_host_is(cls, values: dict) -> Optional[dict]:
-        if values["graylog_host"] and values["graylog_port"] is None:
+        if values.get("graylog_host") and not values.get("graylog_port"):
             raise ValueError("The Graylog port must be set if the Graylog host is")
         return values
 
---
+++ tests/cli/test_start_service.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import sys
+from unittest import mock
+
+from cryoemservices.cli import start_service
+
+
+@mock.patch("workflows.frontend.Frontend")
+def test_start_service(mock_frontend, tmp_path):
+    """Test that wrappers can be started and run"""
+    # Create a sample config file
+    config_file = tmp_path / "config.yaml"
+    with open(config_file, "w") as cf:
+        cf.write("rabbitmq_credentials: rmq_creds\n")
+        cf.write(f"recipe_directory: {tmp_path}\n")
+
+    # Run the wrapper starter
+    sys.argv = [
+        "cryoemservices.wrap",
+        "--service",
+        "MotionCorr",
+        "--config_file",
+        str(config_file),
+    ]
+    start_service.run()
+
+    mock_frontend.assert_called_with(
+        **{
+            "service": "MotionCorr",
+            "transport": mock.ANY,
+            "transport_command_channel": "command",
+            "verbose_service": True,
+            "environment": {},
+        }
+    )
+    mock_frontend().run.assert_called()
