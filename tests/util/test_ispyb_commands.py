7225362b2b3f0cbdefaaf39ec1254a5fd00514a6
yxd92326 <stephen.riggs@diamond.ac.uk>
2024-12-02 15:38:59 +0000

    Testing for tomogram inserts

--- tests/util/test_ispyb_commands.py
+++ tests/util/test_ispyb_commands.py
@@ -469,3 +469,403 @@ def test_insert_particle_classification_group_update():
     )
     mock_session.add.assert_not_called()
     mock_session.commit.assert_called()
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_initial_model_new(mock_models):
+    def mock_model_parameters(p):
+        model_parameters = {
+            "dcid": 10,
+            "cryoem_initial_model_id": None,
+            "particle_classification_id": 401,
+            "resolution": 15.1,
+            "number_of_particles": 21000,
+        }
+        return model_parameters[p]
+
+    mock_session = mock.MagicMock()
+    return_value = ispyb_commands.insert_cryoem_initial_model(
+        {}, mock_model_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    mock_models.CryoemInitialModel.assert_called_with(
+        resolution=15.1,
+        numberOfParticles=21000,
+    )
+    mock_models.t_ParticleClassification_has_CryoemInitialModel.insert().values.assert_called_with(
+        cryoemInitialModelId=mock.ANY,
+        particleClassificationId=401,
+    )
+    mock_session.add.assert_called()
+    mock_session.execute.assert_called()
+    assert mock_session.commit.call_count == 2
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_initial_model_with_id(mock_models):
+    def mock_model_parameters(p):
+        model_parameters = {
+            "dcid": 10,
+            "cryoem_initial_model_id": 601,
+            "particle_classification_id": 401,
+        }
+        return model_parameters[p]
+
+    mock_session = mock.MagicMock()
+    return_value = ispyb_commands.insert_cryoem_initial_model(
+        {}, mock_model_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    mock_models.CryoemInitialModel.assert_not_called()
+    mock_models.t_ParticleClassification_has_CryoemInitialModel.insert().values.assert_called_with(
+        cryoemInitialModelId=601,
+        particleClassificationId=401,
+    )
+    mock_session.add.assert_not_called()
+    mock_session.execute.assert_called()
+    assert mock_session.commit.call_count == 1
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_bfactor_new(mock_models):
+    def mock_bfactor_parameters(p):
+        bfactor_parameters = {
+            "dcid": 10,
+            "bfactor_id": 701,
+            "particle_classification_id": 401,
+            "resolution": 5.2,
+            "number_of_particles": 21000,
+            "particle_batch_size": 50000,
+        }
+        return bfactor_parameters[p]
+
+    # Mock which returns None for existing objects
+    mock_session = mock.MagicMock()
+    mock_session.query().filter().first.return_value = None
+
+    return_value = ispyb_commands.insert_bfactor_fit(
+        {}, mock_bfactor_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    assert mock_session.query.call_count == 2
+    assert mock_session.query().filter.call_count == 2
+    assert mock_session.query().filter().first.call_count == 1
+
+    mock_models.BFactorFit.assert_called_with(
+        bFactorFitId=701,
+        particleClassificationId=401,
+        resolution=5.2,
+        numberOfParticles=21000,
+        particleBatchSize=50000,
+    )
+    mock_session.add.assert_called()
+    mock_session.commit.assert_called()
+
+
+def test_insert_bfactor_update():
+    def mock_bfactor_parameters(p):
+        bfactor_parameters = {
+            "dcid": 10,
+            "bfactor_id": 701,
+            "particle_classification_id": 401,
+            "resolution": 5.2,
+            "number_of_particles": 21000,
+            "particle_batch_size": 50000,
+        }
+        return bfactor_parameters[p]
+
+    # Mock which returns an existing object
+    mock_session = mock.MagicMock()
+    mock_session.query().filter().first.return_value = 1
+
+    return_value = ispyb_commands.insert_bfactor_fit(
+        {}, mock_bfactor_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    assert mock_session.query.call_count == 3
+    assert mock_session.query().filter.call_count == 3
+    assert mock_session.query().filter().first.call_count == 1
+
+    # Don't check the model call here, instead look at the update
+    mock_session.query().filter().update.assert_called_with(
+        {
+            "particleClassificationId": 401,
+            "resolution": 5.2,
+            "numberOfParticles": 21000,
+            "particleBatchSize": 50000,
+        }
+    )
+    mock_session.add.assert_not_called()
+    mock_session.commit.assert_called()
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_tomogram_new(mock_models):
+    def mock_tomogram_parameters(p):
+        tomogram_parameters = {
+            "dcid": 10,
+            "tomogram_id": 801,
+            "program_id": 1,
+            "volume_file": "/path/to/volume",
+            "stack_file": "/path/to/stack",
+            "size_x": 512,
+            "size_y": 400,
+            "size_z": 300,
+            "pixel_spacing": 5.2,
+            "residual_error_mean": 1.3,
+            "residual_error_sd": 0.4,
+            "x_axis_correction": 4.3,
+            "tilt_angle_offset": 1.5,
+            "z_shift": 3.2,
+            "file_directory": "/tomogram/directory",
+            "central_slice_image": "/path/to/central/slice",
+            "tomogram_movie": "/path/to/movie",
+            "xy_shift_plot": "/path/to/shift/plot",
+            "proj_xy": "/path/to/xy",
+            "proj_xz": "/path/to/xz",
+            "alignment_quality": 0.2,
+        }
+        return tomogram_parameters[p]
+
+    # Mock which returns None for existing objects
+    mock_session = mock.MagicMock()
+    mock_session.query().filter().first.return_value = None
+
+    return_value = ispyb_commands.insert_tomogram(
+        {}, mock_tomogram_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    assert mock_session.query.call_count == 2
+    assert mock_session.query().filter.call_count == 2
+    assert mock_session.query().filter().first.call_count == 1
+
+    mock_models.Tomogram.assert_called_with(
+        tomogramId=801,
+        dataCollectionId=10,
+        autoProcProgramId=1,
+        volumeFile="/path/to/volume",
+        stackFile="/path/to/stack",
+        sizeX=512,
+        sizeY=400,
+        sizeZ=300,
+        pixelSpacing=5.2,
+        residualErrorMean=1.3,
+        residualErrorSD=0.4,
+        xAxisCorrection=4.3,
+        tiltAngleOffset=1.5,
+        zShift=3.2,
+        fileDirectory="/tomogram/directory",
+        centralSliceImage="/path/to/central/slice",
+        tomogramMovie="/path/to/movie",
+        xyShiftPlot="/path/to/shift/plot",
+        projXY="/path/to/xy",
+        projXZ="/path/to/xz",
+        globalAlignmentQuality=0.2,
+    )
+    mock_session.add.assert_called()
+    mock_session.commit.assert_called()
+
+
+def test_insert_tomogram_update():
+    def mock_tomogram_parameters(p):
+        tomogram_parameters = {
+            "dcid": 10,
+            "tomogram_id": 801,
+            "program_id": 1,
+            "volume_file": "/path/to/volume",
+            "stack_file": "/path/to/stack",
+            "size_x": 512,
+            "size_y": 400,
+            "size_z": 300,
+            "pixel_spacing": 5.2,
+            "residual_error_mean": 1.3,
+            "residual_error_sd": 0.4,
+            "x_axis_correction": 4.3,
+            "tilt_angle_offset": 1.5,
+            "z_shift": 3.2,
+            "file_directory": "/tomogram/directory",
+            "central_slice_image": "/path/to/central/slice",
+            "tomogram_movie": "/path/to/movie",
+            "xy_shift_plot": "/path/to/shift/plot",
+            "proj_xy": "/path/to/xy",
+            "proj_xz": "/path/to/xz",
+            "alignment_quality": 0.2,
+        }
+        return tomogram_parameters[p]
+
+    # Mock which returns an existing object
+    mock_session = mock.MagicMock()
+    mock_session.query().filter().first.return_value = 1
+
+    return_value = ispyb_commands.insert_tomogram(
+        {}, mock_tomogram_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    assert mock_session.query.call_count == 3
+    assert mock_session.query().filter.call_count == 3
+    assert mock_session.query().filter().first.call_count == 1
+
+    # Don't check the model call here, instead look at the update
+    mock_session.query().filter().update.assert_called_with(
+        {
+            "dataCollectionId": 10,
+            "autoProcProgramId": 1,
+            "volumeFile": "/path/to/volume",
+            "stackFile": "/path/to/stack",
+            "sizeX": 512,
+            "sizeY": 400,
+            "sizeZ": 300,
+            "pixelSpacing": 5.2,
+            "residualErrorMean": 1.3,
+            "residualErrorSD": 0.4,
+            "xAxisCorrection": 4.3,
+            "tiltAngleOffset": 1.5,
+            "zShift": 3.2,
+            "fileDirectory": "/tomogram/directory",
+            "centralSliceImage": "/path/to/central/slice",
+            "tomogramMovie": "/path/to/movie",
+            "xyShiftPlot": "/path/to/shift/plot",
+            "projXY": "/path/to/xy",
+            "projXZ": "/path/to/xz",
+            "globalAlignmentQuality": 0.2,
+        }
+    )
+    mock_session.add.assert_not_called()
+    mock_session.commit.assert_called()
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_processed_tomogram(mock_models):
+    def mock_tomogram_parameters(p):
+        tomogram_parameters = {
+            "dcid": 10,
+            "tomogram_id": 801,
+            "file_path": "/path/to/processed/tomogram",
+            "processing_type": "Denoised",
+        }
+        return tomogram_parameters[p]
+
+    mock_session = mock.MagicMock()
+    return_value = ispyb_commands.insert_processed_tomogram(
+        {}, mock_tomogram_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    mock_models.ProcessedTomogram.assert_called_with(
+        tomogramId=801,
+        filePath="/path/to/processed/tomogram",
+        processingType="Denoised",
+    )
+    mock_session.add.assert_called()
+    mock_session.commit.assert_called()
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_tilts_has_movie(mock_models):
+    def mock_tilt_parameters(p):
+        tilt_parameters = {
+            "dcid": 10,
+            "movie_id": 1,
+            "tomogram_id": 801,
+            "defocus_u": 20000,
+            "defocus_v": 25000,
+            "psd_file": "/path/to/psd/file",
+            "resolution": 10.4,
+            "fit_quality": 0.15,
+            "refined_magnification": 18000,
+            "refined_tilt_angle": -12,
+            "refined_tilt_axis": 83.6,
+            "residual_error": 0.6,
+        }
+        return tilt_parameters[p]
+
+    mock_session = mock.MagicMock()
+    return_value = ispyb_commands.insert_tilt_image_alignment(
+        {}, mock_tilt_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    mock_models.TiltImageAlignment.assert_called_with(
+        movieId=1,
+        tomogramId=801,
+        defocusU=20000,
+        defocusV=25000,
+        psdFile="/path/to/psd/file",
+        resolution=10.4,
+        fitQuality=0.15,
+        refinedMagnification=18000,
+        refinedTiltAngle=-12,
+        refinedTiltAxis=83.6,
+        residualError=0.6,
+    )
+    mock_session.add.assert_called()
+    mock_session.commit.assert_called()
+
+
+@mock.patch("cryoemservices.util.ispyb_commands.models")
+def test_insert_tilts_without_movie(mock_models):
+    def mock_tilt_parameters(p):
+        tilt_parameters = {
+            "dcid": 10,
+            "movie_id": None,
+            "path": "/path/to/test_movie_motion_corrected.mrc",
+            "tomogram_id": 801,
+            "defocus_u": 20000,
+            "defocus_v": 25000,
+            "psd_file": "/path/to/psd/file",
+            "resolution": 10.4,
+            "fit_quality": 0.15,
+            "refined_magnification": 18000,
+            "refined_tilt_angle": -12,
+            "refined_tilt_axis": 83.6,
+            "residual_error": 0.6,
+        }
+        return tilt_parameters[p]
+
+    class MockMovieParameters:
+        movieFullPath = "test_movie.tiff"
+        movieId = 5
+
+    mock_session = mock.MagicMock()
+    mock_session.query().filter().all.return_value = [MockMovieParameters]
+
+    return_value = ispyb_commands.insert_tilt_image_alignment(
+        {}, mock_tilt_parameters, mock_session
+    )
+    assert return_value.get("success")
+    assert return_value["return_value"]
+
+    # Check the movie lookup
+    assert mock_session.query.call_count == 2
+    assert mock_session.query().filter.call_count == 2
+    assert mock_session.query().filter().all.call_count == 1
+
+    mock_models.TiltImageAlignment.assert_called_with(
+        movieId=5,
+        tomogramId=801,
+        defocusU=20000,
+        defocusV=25000,
+        psdFile="/path/to/psd/file",
+        resolution=10.4,
+        fitQuality=0.15,
+        refinedMagnification=18000,
+        refinedTiltAngle=-12,
+        refinedTiltAxis=83.6,
+        residualError=0.6,
+    )
+    mock_session.add.assert_called()
+    mock_session.commit.assert_called()
