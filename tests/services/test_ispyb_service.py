13df25c354cef9165fdd4aa8825aa002f354e77c
yxd92326 <stephen.riggs@diamond.ac.uk>
2024-12-04 09:06:23 +0000

    Test the $ env keys

--- tests/services/test_ispyb_service.py
+++ tests/services/test_ispyb_service.py
@@ -122,6 +122,57 @@ def test_ispyb_service_store_result(
 @mock.patch("cryoemservices.services.ispyb_connector.ispyb.sqlalchemy")
 @mock.patch("cryoemservices.services.ispyb_connector.sqlalchemy")
 @mock.patch("cryoemservices.services.ispyb_connector.ispyb_commands.insert_movie")
+def test_ispyb_service_env_keys(
+    mock_command, mock_sqlalchemy, mock_ispyb_api, offline_transport, tmp_path
+):
+    """
+    Test as above, but read the key values from the environment in $ form
+    """
+    header = {
+        "message-id": mock.sentinel,
+        "subscription": mock.sentinel,
+    }
+    ispyb_test_message = {
+        "parameters": {
+            "ispyb_command": "${command}",
+            "store_result": "$output",
+        },
+        "content": "dummy",
+    }
+    ispyb_test_environment = {"command": "insert_movie", "output": "full_result"}
+
+    mock_command.return_value = {
+        "success": True,
+        "return_value": "result_value",
+        "store_result": "result_name",
+    }
+
+    mock_rw = mock.MagicMock()
+    mock_rw.recipe_step = {"parameters": ispyb_test_message["parameters"]}
+    mock_rw.environment = ispyb_test_environment
+
+    # Set up the mock service and call it
+    service = ispyb_connector.EMISPyB()
+    service.transport = offline_transport
+    service.start()
+    service.insert_into_ispyb(rw=mock_rw, header=header, message=ispyb_test_message)
+
+    # Check that the correct messages were sent
+    mock_command.assert_called()
+    mock_rw.send_to.assert_called_with(
+        "output",
+        {"result": "result_value"},
+    )
+
+    # Check the results were stored in the environment
+    assert mock_rw.environment["full_result"] == "result_value"
+    assert mock_rw.environment["result_name"] == "result_value"
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
+@mock.patch("cryoemservices.services.ispyb_connector.ispyb.sqlalchemy")
+@mock.patch("cryoemservices.services.ispyb_connector.sqlalchemy")
+@mock.patch("cryoemservices.services.ispyb_connector.ispyb_commands.insert_movie")
 def test_ispyb_service_checkpoint(
     mock_command, mock_sqlalchemy, mock_ispyb_api, offline_transport, tmp_path
 ):
