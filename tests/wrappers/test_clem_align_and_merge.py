e8805412a3f8405d4ffd25133cfe992cf556cffa
Tien, Eu Pin <48775880+tieneupin@users.noreply.github.com>
2024-12-20 14:50:38 +0000

    Added integrated tests for CLEM workflow wrappers (LIF and TIFF preprocessing) (#102)

--- tests/wrappers/test_clem_align_and_merge.py
+++ tests/wrappers/test_clem_align_and_merge.py
@@ -9,7 +9,7 @@ from workflows.transport.offline_transport import OfflineTransport
 from cryoemservices.wrappers.clem_align_and_merge import AlignAndMergeWrapper
 
 
-# Sets up a mock transport object
+# Set up a mock transport object
 @pytest.fixture
 def offline_transport(mocker):  # 'mocker' is a keyword associated with unittest.mock
     transport = OfflineTransport()
@@ -25,27 +25,30 @@ def offline_transport(mocker):  # 'mocker' is a keyword associated with unittest
 def test_align_and_merge_wrapper(
     mock_align_and_merge,
     mock_send_to,
-    offline_transport,  # The fixture defined earlier
-    tmp_path,  # Where pytest runs by default
+    offline_transport,  # Fixture defined above
+    tmp_path,  # Pytest default working directory
 ):
 
-    # Feed it a dictionary
+    # Construct a dictionary to pass to the wrapper
+    image_list = [tmp_path / f"{color}.tiff" for color in ("gray", "green", "red")]
+    metadata = tmp_path / "metadata" / "test_series.xml"
+    crop_to_n_frames = 30
+    align_self = "enabled"
+    flatten = "max"
+    align_across = "enabled"
+
     message = {
         "recipe": {
             "start": [[1, []]],
             "1": {
                 "job_parameters": {
                     "series_name": "test_series",
-                    "images": [
-                        f"{tmp_path}/gray.tiff",
-                        f"{tmp_path}/green.tiff",
-                        f"{tmp_path}/red.tiff",
-                    ],
-                    "metadata": f"{tmp_path}/metadata/test_series.xml",
-                    "crop_to_n_frames": 30,
-                    "align_self": "enabled",
-                    "flatten": "max",
-                    "align_across": "enabled",
+                    "images": [str(image) for image in image_list],
+                    "metadata": str(metadata),
+                    "crop_to_n_frames": crop_to_n_frames,
+                    "align_self": align_self,
+                    "flatten": flatten,
+                    "align_across": align_across,
                 },
                 "parameters": {},
             },
@@ -57,39 +60,33 @@ def test_align_and_merge_wrapper(
     # Set a recipe wrapper with the correct dictionaries
     recipe_wraper = RecipeWrapper(message=message, transport=offline_transport)
 
-    # Manually start up a recipe wrapper
+    # Manually start up the function wrapper
     align_and_merge_wrapper = AlignAndMergeWrapper()
     align_and_merge_wrapper.set_recipe_wrapper(recipe_wraper)
     result = align_and_merge_wrapper.run()
 
     # Start checking the calls that take place within the function
-
-    # Step 1: Validating the message via Pydantic
-    # This can be skipped
-
-    # Step 2: Check the align and merge function
-    mock_align_and_merge.assert_called_once()
-    mock_align_and_merge.assert_called_with(
-        images=[tmp_path / "gray.tiff", tmp_path / "green.tiff", tmp_path / "red.tiff"],
-        metadata=tmp_path / "metadata/test_series.xml",
-        crop_to_n_frames=30,
-        align_self="enabled",
-        flatten="max",
-        align_across="enabled",
+    # Check the align-and-merge function is called correctly
+    mock_align_and_merge.assert_called_once_with(
+        images=image_list,
+        metadata=metadata,
+        crop_to_n_frames=crop_to_n_frames,
+        align_self=align_self,
+        flatten=flatten,
+        align_across=align_across,
     )
 
-    # Step 3: Check that the wrapper sends the correct output message
-    mock_send_to.assert_called_once()  # Check that it's only called once
-
-    # Generate the dictionary we expect to get
+    # Check that the wrapper sends the correct output message
+    # Generate the dictionary to be sent out
     murfey_params = {
         "register": "clem.register_align_and_merge_result",
         "result": mock.ANY,  # Checks that an object exists
     }
-    mock_send_to.assert_called_with(
+    # Check that the message is sent out correctly
+    mock_send_to.assert_called_once_with(
         "murfey_feedback",
         murfey_params,
     )
 
-    # Check at the end that wrapper ran to completion successfully
+    # Check that the wrapper ran through to completion
     assert result
