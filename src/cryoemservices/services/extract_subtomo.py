f4609f42348663f894722421238b5ece1339db4b
yxd92326 <stephen.riggs@diamond.ac.uk>
2025-03-10 09:47:09 +0000

    Possible subtomogram extraction service

---
+++ src/cryoemservices/services/extract_subtomo.py
@@ -0,0 +1,347 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import mrcfile
+import numpy as np
+import workflows.recipe
+from gemmi import cif
+from pydantic import BaseModel, Field, ValidationError
+from workflows.services.common_service import CommonService
+
+from cryoemservices.util.models import MockRW
+from cryoemservices.util.relion_service_options import (
+    RelionServiceOptions,
+    update_relion_options,
+)
+from cryoemservices.util.tomo_output_files import _get_tilt_name_v5_12
+
+
+class ExtractSubTomoParameters(BaseModel):
+    tomogram: str = Field(..., min_length=1)
+    coord_list_file: str = Field(..., min_length=1)
+    tilt_alignment_file: str = Field(..., min_length=1)
+    newstack_file: str = Field(..., min_length=1)
+    output_file: str = Field(..., min_length=1)
+    pixel_size: float
+    refined_tilt_axis: float
+    particle_diameter: float = 0
+    boxsize: int = 256
+    small_boxsize: int = 64
+    binning: int = 8
+    min_frames: int = 1
+    maximum_dose: int = -1
+    tomogram_binning: int = 4
+    invert_contrast: bool = True
+    bg_radius: int = -1
+    relion_options: RelionServiceOptions
+
+
+class ExtractSubTomo(CommonService):
+    """
+    A service for extracting particles from cryolo autopicking for tomograms
+    """
+
+    # Human readable service name
+    _service_name = "ExtractSubTomo"
+
+    # Logger name
+    _logger_name = "cryoemservices.services.extract_subtomo"
+
+    # Job name
+    job_type = "relion.pseudosubtomo"
+
+    def initializing(self):
+        """Subscribe to a queue. Received messages must be acknowledged."""
+        self.log.info("Extract service starting")
+        workflows.recipe.wrap_subscribe(
+            self._transport,
+            self._environment["queue"] or "extract_subtomo",
+            self.extract_subtomo,
+            acknowledgement=True,
+            log_extender=self.extend_log,
+            allow_non_recipe_messages=True,
+        )
+
+    def extract_subtomo(self, rw, header: dict, message: dict):
+        """Main function which interprets and processes received messages"""
+        if not rw:
+            self.log.info("Received a simple message")
+            if not isinstance(message, dict):
+                self.log.error("Rejected invalid simple message")
+                self._transport.nack(header)
+                return
+
+            # Create a wrapper-like object that can be passed to functions
+            # as if a recipe wrapper was present.
+            rw = MockRW(self._transport)
+            rw.recipe_step = {"parameters": message}
+
+        try:
+            if isinstance(message, dict):
+                extract_subtomo_params = ExtractSubTomoParameters(
+                    **{**rw.recipe_step.get("parameters", {}), **message}
+                )
+            else:
+                extract_subtomo_params = ExtractSubTomoParameters(
+                    **{**rw.recipe_step.get("parameters", {})}
+                )
+        except (ValidationError, TypeError) as e:
+            self.log.warning(
+                f"Extraction parameter validation failed for message: {message} "
+                f"and recipe parameters: {rw.recipe_step.get('parameters', {})} "
+                f"with exception: {e}"
+            )
+            rw.transport.nack(header)
+            return
+
+        self.log.info(
+            f"Inputs: {extract_subtomo_params.tomogram}, "
+            f"{extract_subtomo_params.tilt_alignment_file}, "
+            f"{extract_subtomo_params.coord_list_file} "
+            f"Output: {extract_subtomo_params.output_file}"
+        )
+
+        # Update the relion options and get box sizes
+        extract_subtomo_params.relion_options = update_relion_options(
+            extract_subtomo_params.relion_options, dict(extract_subtomo_params)
+        )
+
+        # Make sure the output directory exists
+        if not Path(extract_subtomo_params.output_file).parent.exists():
+            Path(extract_subtomo_params.output_file).parent.mkdir(parents=True)
+
+        # If no background radius set diameter as 75% of box
+        if extract_subtomo_params.bg_radius == -1:
+            extract_subtomo_params.bg_radius = round(
+                0.375 * extract_subtomo_params.relion_options.small_boxsize
+            )
+
+        # Find the locations of the particles
+        coords_file = cif.read(extract_subtomo_params.coord_list_file)
+        coords_block = coords_file.find_block("cryolo")
+        particles_x = np.array(coords_block.find_loop("_CoordinateX"))
+        particles_y = np.array(coords_block.find_loop("_CoordinateY"))
+        particles_z = np.array(coords_block.find_loop("_CoordinateZ"))
+
+        # Get the shifts between tilts
+        shift_data = np.genfromtxt(extract_subtomo_params.tilt_alignment_file)
+        # tilt_ids = shift_data[:, 0].astype(int)
+        x_shifts = shift_data[:, 3].astype(float)
+        y_shifts = shift_data[:, 4].astype(float)
+        tilt_count = len(x_shifts)
+
+        # Extraction
+        with mrcfile.open(extract_subtomo_params.tomogram) as input_tomogram:
+            input_tomogram_image = np.array(input_tomogram.data, dtype=np.float32)
+        image_size = np.shape(input_tomogram_image)
+
+        centre_x = 0
+        centre_y = image_size[1] / 2
+        tilt_axis_radians = extract_subtomo_params.refined_tilt_axis * np.pi / 180
+
+        x_coords_in_tilts = centre_x + (
+            (particles_x - centre_x) * np.cos(tilt_axis_radians)
+            - (particles_y - centre_y) * np.sin(tilt_axis_radians)
+        )
+        y_coords_in_tilts = centre_y + (
+            (particles_x - centre_x) * np.sin(tilt_axis_radians)
+            + (particles_y - centre_y) * np.cos(tilt_axis_radians)
+        )
+
+        # Downscaling dimensions
+        extract_subtomo_params.relion_options.pixel_size_downscaled = (
+            extract_subtomo_params.pixel_size
+            * extract_subtomo_params.relion_options.boxsize
+            / extract_subtomo_params.relion_options.small_boxsize
+        )
+        extract_width = round(extract_subtomo_params.relion_options.boxsize / 2)
+        scaled_extract_width = round(
+            extract_subtomo_params.relion_options.small_boxsize / 2
+        )
+        box_len = extract_subtomo_params.relion_options.small_boxsize
+        pixel_size = extract_subtomo_params.relion_options.pixel_size_downscaled
+
+        # Read in tilt images
+        tilt_images = []
+        with open(extract_subtomo_params.newstack_file) as ns_file:
+            while True:
+                line = ns_file.readline()
+                if not line:
+                    break
+                elif line.startswith("/"):
+                    tilt_images.append(line)
+
+        for particle in range(len(particles_x)):
+            output_mrc_stack = np.array([])
+            for tilt in range(tilt_count):
+                # Extract the particle image and pad the edges if it is not square
+                x_left_pad = 0
+                x_right_pad = 0
+                y_top_pad = 0
+                y_bot_pad = 0
+
+                x_left = (
+                    x_coords_in_tilts[particle] - extract_width - x_shifts[particle]
+                )
+                if x_left < 0:
+                    x_left_pad = -x_left
+                    x_left = 0
+                x_right = (
+                    x_coords_in_tilts[particle] + extract_width - x_shifts[particle]
+                )
+                if x_right >= image_size[1]:
+                    x_right_pad = x_right - image_size[1]
+                    x_right = image_size[1]
+                y_top = y_coords_in_tilts[particle] - extract_width - y_shifts[particle]
+                if y_top < 0:
+                    y_top_pad = -y_top
+                    y_top = 0
+                y_bot = y_coords_in_tilts[particle] + extract_width - y_shifts[particle]
+                if y_bot >= image_size[0]:
+                    y_bot_pad = y_bot - image_size[0]
+                    y_bot = image_size[0]
+
+                with mrcfile.open(tilt_images[tilt]) as mrc:
+                    input_micrograph_image = mrc.data
+
+                particle_subimage = input_micrograph_image[y_top:y_bot, x_left:x_right]
+                particle_subimage = np.pad(
+                    particle_subimage,
+                    ((y_bot_pad, y_top_pad), (x_left_pad, x_right_pad)),
+                    mode="edge",
+                )
+
+                # Flip all the values on inversion
+                if extract_subtomo_params.invert_contrast:
+                    particle_subimage = -1 * particle_subimage
+
+                # Downscale the image size
+                subimage_ft = np.fft.fftshift(np.fft.fft2(particle_subimage))
+                deltax = (
+                    subimage_ft.shape[0]
+                    - extract_subtomo_params.relion_options.small_boxsize
+                )
+                deltay = (
+                    subimage_ft.shape[1]
+                    - extract_subtomo_params.relion_options.small_boxsize
+                )
+                particle_subimage = np.real(
+                    np.fft.ifft2(
+                        np.fft.ifftshift(
+                            subimage_ft[
+                                deltax // 2 : subimage_ft.shape[0] - deltax // 2,
+                                deltay // 2 : subimage_ft.shape[1] - deltay // 2,
+                            ]
+                        )
+                    )
+                )
+
+                # Distance of each pixel from the centre, compared to background radius
+                grid_indexes = np.meshgrid(
+                    np.arange(2 * scaled_extract_width),
+                    np.arange(2 * scaled_extract_width),
+                )
+                distance_from_centre = np.sqrt(
+                    (grid_indexes[0] - scaled_extract_width + 0.5) ** 2
+                    + (grid_indexes[1] - scaled_extract_width + 0.5) ** 2
+                )
+                bg_region = (
+                    distance_from_centre
+                    > np.ones(np.shape(particle_subimage))
+                    * extract_subtomo_params.bg_radius
+                )
+
+                # Background normalisation
+                bg_mean = np.mean(particle_subimage[bg_region])
+                bg_std = np.std(particle_subimage[bg_region])
+                particle_subimage = (particle_subimage - bg_mean) / bg_std
+
+                # Add to output stack
+                if len(output_mrc_stack):
+                    output_mrc_stack = np.append(
+                        output_mrc_stack, [particle_subimage], axis=0
+                    )
+                else:
+                    output_mrc_stack = np.array([particle_subimage], dtype=np.float32)
+
+            # Produce the mrc file of the extracted particles
+            output_mrc_file = (
+                Path(extract_subtomo_params.output_file).parent
+                / f"{particle}_stack2d.mrcs"
+            )
+            self.log.info(f"Extracted particle {particle}")
+            with mrcfile.new(str(output_mrc_file), overwrite=True) as mrc:
+                mrc.set_data(output_mrc_stack.astype(np.float32))
+                mrc.header.mx = box_len
+                mrc.header.my = box_len
+                mrc.header.mz = 1
+                mrc.header.cella.x = pixel_size * box_len
+                mrc.header.cella.y = pixel_size * box_len
+                mrc.header.cella.z = 1
+
+        # Construct the output star file
+        extracted_parts_doc = cif.Document()
+        extracted_parts_block = extracted_parts_doc.add_new_block("particles")
+        extracted_parts_loop = extracted_parts_block.init_loop(
+            "_rln",
+            [
+                "CenteredCoordinateXAngst",
+                "CenteredCoordinateYAngst",
+                "CenteredCoordinateZAngst",
+                "OpticsGroup",
+                "TomoParticleName",
+                "TomoVisibleFrames",
+                "ImageName",
+                "OriginXAngst",
+                "OriginYAngst",
+                "OriginZAngst",
+            ],
+        )
+        frames = "?????"
+        for particle in range(len(particles_x)):
+            extracted_parts_loop.add_row(
+                [
+                    str(
+                        float(particles_x[particle])
+                        - image_size[2] / 2 * extract_subtomo_params.tomogram_binning
+                    ),
+                    str(
+                        float(particles_y[particle])
+                        - image_size[1] / 2 * extract_subtomo_params.tomogram_binning
+                    ),
+                    str(
+                        float(particles_z[particle])
+                        - image_size[0] / 2 * extract_subtomo_params.tomogram_binning
+                    ),
+                    "1",
+                    f"{_get_tilt_name_v5_12(Path(extract_subtomo_params.tomogram))}/{particle}",
+                    f"[{frames}]"
+                    f"{Path(extract_subtomo_params.output_file).parent}/{particle}_stack2d.mrcs",
+                    "0.0",
+                    "0.0",
+                    "0.0",
+                ]
+            )
+        extracted_parts_doc.write_file(
+            extract_subtomo_params.output_file, style=cif.Style.Simple
+        )
+
+        # Register the extract job with the node creator
+        self.log.info(f"Sending {self.job_type} to node creator")
+        node_creator_parameters = {
+            "job_type": self.job_type,
+            "input_file": extract_subtomo_params.coord_list_file,
+            "output_file": extract_subtomo_params.output_file,
+            "relion_options": dict(extract_subtomo_params.relion_options),
+            "command": "",
+            "stdout": "",
+            "stderr": "",
+            "results": {"box_size": box_len},
+        }
+        rw.send_to("node_creator", node_creator_parameters)
+
+        self.log.info(
+            f"Done {self.job_type} for {extract_subtomo_params.coord_list_file}."
+        )
+        rw.transport.ack(header)
