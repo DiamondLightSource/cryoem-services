e048fc9575a763729a31a0e71b549aeb30d46d41
yxd92326 <stephen.riggs@diamond.ac.uk>
2025-06-02 15:00:31 +0100

    Remove the extra layer of multiprocessing when starting services

--- tests/cli/test_start_service.py
+++ tests/cli/test_start_service.py
@@ -8,14 +8,9 @@ from cryoemservices.cli import start_service
 
 
 @mock.patch("cryoemservices.cli.start_service.PikaTransport")
-@mock.patch("cryoemservices.cli.start_service.multiprocessing.Process")
 @mock.patch("cryoemservices.services.motioncorr.MotionCorr")
-def test_start_service_with_optional_args(
-    mock_service, mock_multiprocess, mock_transport, tmp_path
-):
+def test_start_service_with_optional_args(mock_service, mock_transport, tmp_path):
     """Test that wrappers can be started and run"""
-    mock_multiprocess().is_alive.side_effect = [True, False]
-
     # Create a sample config file
     config_file = tmp_path / "config.yaml"
     with open(config_file, "w") as cf:
@@ -39,9 +34,6 @@ def test_start_service_with_optional_args(
     # Check the calls which should be made
     mock_transport.assert_called()
     mock_transport().load_configuration_file.assert_called_with(Path("rmq_creds"))
-    mock_transport().connect.assert_called()
-    mock_transport().disconnect.assert_called()
-    mock_transport().is_connected.assert_called()
 
     mock_service.assert_called_with(
         environment={
@@ -51,22 +43,13 @@ def test_start_service_with_optional_args(
         },
         transport=mock.ANY,
     )
-
-    mock_multiprocess.assert_called_with(target=mock.ANY)
-    mock_multiprocess().start.assert_called()
-    mock_multiprocess().is_alive.assert_called()
-    mock_multiprocess().terminate.assert_called()
-    mock_multiprocess().join.assert_called()
+    mock_service().start.assert_called_once()
 
 
 @mock.patch("cryoemservices.cli.start_service.PikaTransport")
-@mock.patch("cryoemservices.cli.start_service.multiprocessing.Process")
 @mock.patch("cryoemservices.services.motioncorr.MotionCorr")
-def test_start_service_with_default_args(
-    mock_service, mock_multiprocess, mock_transport, tmp_path
-):
+def test_start_service_with_default_args(mock_service, mock_transport, tmp_path):
     """Test that wrappers can be started and run"""
-    mock_multiprocess().is_alive.side_effect = [True, False]
 
     # Create a sample config file
     config_file = tmp_path / "config.yaml"
@@ -87,9 +70,6 @@ def test_start_service_with_default_args(
     # Check the calls which should be made
     mock_transport.assert_called()
     mock_transport().load_configuration_file.assert_called_with(Path("rmq_creds"))
-    mock_transport().connect.assert_called()
-    mock_transport().disconnect.assert_called()
-    mock_transport().is_connected.assert_called()
 
     mock_service.assert_called_with(
         environment={
@@ -99,9 +79,4 @@ def test_start_service_with_default_args(
         },
         transport=mock.ANY,
     )
-
-    mock_multiprocess.assert_called_with(target=mock.ANY)
-    mock_multiprocess().start.assert_called()
-    mock_multiprocess().is_alive.assert_called()
-    mock_multiprocess().terminate.assert_called()
-    mock_multiprocess().join.assert_called()
+    mock_service().start.assert_called_once()
