cdc572607f1e89c28f5fc389cb0a4a7878092da6
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-03 16:37:59 +0100

    Added test for when Pydantic validation fails

--- tests/services/test_clem_process_raw_lifs_service.py
+++ tests/services/test_clem_process_raw_lifs_service.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Any
 from unittest import mock
 
 import pytest
@@ -163,7 +164,7 @@ def test_lif_to_stack_service(
         )
 
 
-def test_align_and_merge_bad_messsage(
+def test_lif_to_stack_bad_messsage(
     offline_transport: OfflineTransport,
 ):
     # Set up the parameters
@@ -186,4 +187,60 @@ def test_align_and_merge_bad_messsage(
     )
 
     # Check that message was nacked with the expected parameters
+    offline_transport.nack.assert_called_once_with(header)
+
+
+# Introduce invalid parameters field-by-field
+lif_to_stack_params_bad_validation_matrix = (
+    # LIF file | Root folder | Num procs
+    (False, True, True),
+    (True, False, True),
+    (True, True, False),
+)
+
+
+@pytest.mark.parametrize("test_params", lif_to_stack_params_bad_validation_matrix)
+def test_lif_to_stack_service_validation_failed(
+    test_params: tuple[bool, bool, bool],
+    lif_file: Path,
+    raw_dir: Path,
+    offline_transport: OfflineTransport,
+):
+    """
+    Sends a test message to the image alignment and merging service, which should
+    excecute the function using the parameters present in the message, then send
+    messages with the expected outputs back to Murfey.
+    """
+
+    # Unpack test params
+    (
+        valid_lif_file,
+        valid_root_folder,
+        valid_num_procs,
+    ) = test_params
+
+    # Set up the parameters
+    header = {
+        "message-id": mock.sentinel,
+        "subscription": mock.sentinel,
+    }
+    lif_file_value: Any = str(lif_file) if valid_lif_file else 123456789
+    root_folder: Any = raw_dir.stem if valid_root_folder else 123456789
+    num_procs: Any = 20 if valid_num_procs else "This is a string"
+    lif_to_stack_test_message = {
+        "lif_file": lif_file_value,
+        "root_folder": root_folder,
+        "num_procs": num_procs,
+    }
+
+    # Set up and run the service
+    service = LIFToStackService(environment={"queue": ""}, transport=offline_transport)
+    service.initializing()
+    service.call_process_raw_lifs(
+        None,
+        header=header,
+        message=lif_to_stack_test_message,
+    )
+
+    # Check that the message was nacked with the expected parameters
     offline_transport.nack.assert_called_once_with(header)
