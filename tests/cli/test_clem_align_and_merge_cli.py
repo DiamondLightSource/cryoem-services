fb5009f57cc4d97e25be080c5c8ccbb594581738
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-11 18:10:35 +0100

    Updated tests for image alignemnt and merging CLI to account for changed behaviour

--- tests/cli/test_clem_align_and_merge_cli.py
+++ tests/cli/test_clem_align_and_merge_cli.py
@@ -2,51 +2,13 @@ from __future__ import annotations
 
 import subprocess
 import sys
-from pathlib import Path
 from unittest import mock
 
 from cryoemservices.cli import clem_align_and_merge
 
 
-@mock.patch("cryoemservices.cli.clem_align_and_merge.align_and_merge_stacks")
-def test_align_and_merge_with_optional_args(mock_align_and_merge, tmp_path):
-    """Test that the cli runs with all args provided"""
-    (tmp_path / "file1").touch()
-    (tmp_path / "file2").touch()
-
-    # Run the cli
-    sys.argv = [
-        "clem.align_and_merge",
-        f"{tmp_path}/file1",
-        f"{tmp_path}/file2",
-        "--metadata",
-        "metadata.xml",
-        "--crop-to-n-frames",
-        "5",
-        "--align-self",
-        "enabled",
-        "--flatten",
-        "min",
-        "--align-across",
-        "enabled",
-        "--debug",
-    ]
-    clem_align_and_merge.run()
-
-    mock_align_and_merge.assert_called_with(
-        images=[tmp_path / "file1", tmp_path / "file2"],
-        metadata=Path("metadata.xml"),
-        crop_to_n_frames=5,
-        align_self="enabled",
-        flatten="min",
-        align_across="enabled",
-        print_messages=True,
-        debug=True,
-    )
-
-
-@mock.patch("cryoemservices.cli.clem_align_and_merge.align_and_merge_stacks")
-def test_align_and_merge_with_default_args(mock_align_and_merge, tmp_path):
+@mock.patch("cryoemservices.wrappers.clem_align_and_merge.align_and_merge_stacks")
+def test_align_and_merge(mock_align_and_merge, tmp_path):
     """Test that the cli runs the expected default args"""
     (tmp_path / "file1").touch()
 
@@ -64,12 +26,10 @@ def test_align_and_merge_with_default_args(mock_align_and_merge, tmp_path):
         align_self="",
         flatten="mean",
         align_across="",
-        print_messages=True,
-        debug=False,
     )
 
 
-@mock.patch("cryoemservices.cli.clem_align_and_merge.align_and_merge_stacks")
+@mock.patch("cryoemservices.wrappers.clem_align_and_merge.align_and_merge_stacks")
 def test_align_and_merge_string_list(mock_align_and_merge, tmp_path):
     """Test that the cli runs the expected default args"""
     (tmp_path / "file1").touch()
@@ -89,8 +49,6 @@ def test_align_and_merge_string_list(mock_align_and_merge, tmp_path):
         align_self="",
         flatten="mean",
         align_across="",
-        print_messages=True,
-        debug=False,
     )
 
 
