26cff689f1bd570ce3e603ab26fbf80d2bf9b952
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-11 18:42:09 +0100

    Added logic to print logs to console when running LIF processing workflow as CLI

--- src/cryoemservices/cli/clem_lif_to_stack.py
+++ src/cryoemservices/cli/clem_lif_to_stack.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import argparse
 from pathlib import Path
 
-from cryoemservices.cli import LineWrapHelpFormatter
-from cryoemservices.wrappers.clem_process_raw_lifs import convert_lif_to_stack
+from cryoemservices.cli import LineWrapHelpFormatter, set_up_logging
 
 
 def run():
@@ -32,7 +31,17 @@ def run():
         default=1,
         help="Number of processes to run",
     )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Print additional messages to check functions work as intended",
+    )
     args = parser.parse_args()
+
+    # Set up the logger before importing the module functions
+    set_up_logging(debug=args.debug)
+
+    from cryoemservices.wrappers.clem_process_raw_lifs import convert_lif_to_stack
 
     # Run function
     results = convert_lif_to_stack(
