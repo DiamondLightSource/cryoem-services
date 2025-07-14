7beeb45b0a20deacdfd8a5687f63a54edfc88caf
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-14 10:48:53 +0100

    Re-added test to run LIF processing CLI with default args; added check that logs were set up correctly

--- tests/cli/test_clem_lif_to_stack_cli.py
+++ tests/cli/test_clem_lif_to_stack_cli.py
@@ -9,13 +9,14 @@ from pytest_mock import MockerFixture
 from cryoemservices.cli import clem_lif_to_stack
 
 
-def test_lif_to_stack(mocker: MockerFixture):
+def test_lif_to_stack_with_optional_args(mocker: MockerFixture):
     """Test that the cli runs with all args provided"""
     # Set up necessary mock objects
     mock_convert = mocker.patch(
         "cryoemservices.wrappers.clem_process_raw_lifs.convert_lif_to_stack"
     )
     mock_print = mocker.patch("cryoemservices.cli.clem_lif_to_stack.print")
+    mock_setup = mocker.patch("cryoemservices.cli.clem_lif_to_stack.set_up_logging")
 
     # Create some dummy results
     dummy_results = [{"result": i} for i in range(3)]
@@ -34,6 +35,7 @@ def test_lif_to_stack(mocker: MockerFixture):
     clem_lif_to_stack.run()
 
     # Check that it was called with the correct args
+    mock_setup.assert_called_with(debug=True)
     mock_convert.assert_called_with(
         file=Path("file.lif"),
         root_folder="root",
@@ -43,6 +45,34 @@ def test_lif_to_stack(mocker: MockerFixture):
     # Check that the dummy results are printed at the end
     for result in dummy_results:
         mock_print.assert_any_call(result)
+
+
+def test_lif_to_stack_with_default_args(mocker: MockerFixture):
+    """Test that the cli runs with all args provided"""
+    # Set up necessary mock objects
+    mock_convert = mocker.patch(
+        "cryoemservices.wrappers.clem_process_raw_lifs.convert_lif_to_stack"
+    )
+    mock_setup = mocker.patch("cryoemservices.cli.clem_lif_to_stack.set_up_logging")
+
+    # Create some dummy results
+    dummy_results = [{"result": i} for i in range(3)]
+    mock_convert.return_value = dummy_results
+
+    # Run the cli
+    sys.argv = [
+        "clem.lif_to_stack",
+        "file.lif",
+    ]
+    clem_lif_to_stack.run()
+
+    # Check that it was called with the correct args
+    mock_setup.assert_called_with(debug=False)
+    mock_convert.assert_called_with(
+        file=Path("file.lif"),
+        root_folder="images",
+        number_of_processes=1,
+    )
 
 
 def test_lif_to_stack_exists():
