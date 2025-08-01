7c9e351d08271e26c691527950fad463bec4f0d5
Stephen Riggs <122790971+stephen-riggs@users.noreply.github.com>
2025-08-01 11:34:11 +0100

    Adjustments to star files written by tomography (#182)
    We're not currently matching relion 5 in all the tomography star files. This addresses some issues, although there may be more to find.
    
    -   Input to ExcludeTiltSeries should be CtfFind
    -   ExcludeTiltSeries jobs should have CTF outputs in the tilt series star files
    -   If CTF output does not exist, do not make star files (in future we could include ctf directly in the workflow to ensure this is true)
    -   Inputs will be relative to /.../project/MotionCorr, not just MotionCorr

--- src/cryoemservices/services/tomo_align.py
+++ src/cryoemservices/services/tomo_align.py
@@ -523,9 +523,17 @@ class TomoAlign(CommonService):
         (project_dir / f"ExcludeTiltImages/job{job_number - 2:03}").mkdir(
             parents=True, exist_ok=True
         )
+        if not (
+            project_dir / f"ExcludeTiltImages/job{job_number - 2:03}/tilts"
+        ).exists():
+            (
+                project_dir / f"ExcludeTiltImages/job{job_number - 2:03}/tilts"
+            ).symlink_to(project_dir / "MotionCorr/job002/Movies")
         (project_dir / f"AlignTiltSeries/job{job_number - 1:03}").mkdir(
             parents=True, exist_ok=True
         )
+        if self.rot:
+            tomo_params.relion_options.tilt_axis_angle = self.rot
         for im, movie in enumerate(self.input_file_list_of_lists):
             if im + 1 in missing_indices:
                 im_diff += 1
@@ -549,7 +557,9 @@ class TomoAlign(CommonService):
                         {
                             "job_type": "relion.excludetilts",
                             "experiment_type": "tomography",
-                            "input_file": str(movie[0]),
+                            "input_file": movie[0].replace(
+                                "MotionCorr/job002", "CtfFind/job003"
+                            ),
                             "output_file": str(
                                 project_dir
                                 / f"ExcludeTiltImages/job{job_number - 2:03}/tilts"
