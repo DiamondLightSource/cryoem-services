11797d1c5dbb2eec579764827f962aa2a1400753
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-08-05 12:10:12 +0100

    Added CLEM metadata function to extract dimension information from the XML file

--- src/cryoemservices/util/clem_metadata.py
+++ src/cryoemservices/util/clem_metadata.py
@@ -15,7 +15,7 @@ from __future__ import annotations
 
 import logging
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 from xml.etree import ElementTree as ET
 
 logger = logging.getLogger("cryoemservice.util.clem_raw_metadata")
@@ -53,6 +53,79 @@ def find_image_elements(
     for child in node:
         find_image_elements(child, path, result)
     return result
+
+
+def get_dimensions(node: ET.Element) -> dict:
+    """
+    Parses the XML metadata from a single dataset (this will raise an error
+    if the XML metadata contains multiple datasets) to calculate and return
+    dimension information.
+
+    This information will include the length, pixel size, resolution, and
+    units of the x, y, and z axes, along with the number of images present
+    in an overview.
+
+    """
+
+    dimensions_key = (
+        ("1", "x"),
+        ("2", "y"),
+        ("3", "z"),
+        ("10", "m"),
+    )
+
+    dims = node.findall(".//DimensionDescription")
+
+    dims_info: dict[str, Any] = {}
+    for dim_id, dim_name in dimensions_key:
+        # Check that the metadata is for one dataset only
+        search_results = [dim for dim in dims if dim.get("DimID") == dim_id]
+        if len(search_results) > 1:
+            raise ValueError(
+                f"More than one node found for the {dim_name}-axis. "
+                "Metadata for multiple datasets are likely present."
+            )
+        if len(search_results) == 0:
+            dims_info[dim_name] = {}
+            continue
+        dim_info = search_results[0]
+        num_elements = int(dim_info.get("NumberOfElements", ""))
+
+        # Handle x, y, and z axes differently
+        if dim_id != "10":
+            origin = float(dim_info.get("Origin", ""))
+            length = float(dim_info.get("Length", ""))
+            units: str = dim_info.get("Unit", "")
+
+            # Get the end-to-end length of the axis
+            # 'Length' is midpoint-to-midpoint length
+            end_to_end_length = (length - origin) * num_elements / (num_elements - 1)
+
+            # Calculate resolution and pixel size
+            resolution = num_elements / end_to_end_length  # Pixels per unit
+            pixel_size = 1 / resolution  # Units per pixel
+
+            # Update dictionary
+            dims_info[dim_name] = (
+                {
+                    "num_frames": num_elements,
+                }
+                if dim_id == "3"
+                else {
+                    "num_pixels": num_elements,
+                }
+            )
+            dims_info[dim_name].update(
+                {
+                    "length": end_to_end_length,
+                    "units": units,
+                    "resolution": resolution,
+                    "pixel_size": pixel_size,
+                }
+            )
+        else:
+            dims_info[dim_name] = {"num_tiles": num_elements}
+    return dims_info
 
 
 def get_axis_resolution(node: ET.Element) -> float:
