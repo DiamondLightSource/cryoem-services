552454356e58e08045e1827dc42d7c18c848dea4
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-10-21 17:49:03 +0100

    add score calculating method to distribute selections across an embedding space

--- src/cryoemservices/util/embeddings.py
+++ src/cryoemservices/util/embeddings.py
@@ -6,3 +6,20 @@ def distance_matrix(coords: np.array) -> np.array:
     for i, xs in enumerate(coords):
         diffs[i] = np.subtract.outer(xs, xs)
     return np.sqrt(np.sum(diffs**2, axis=0))
+
+
+def distance_exp_score(selection_hist: np.array, distances: np.array) -> float:
+    if len(selection_hist) != len(distances):
+        raise ValueError(
+            f"Size of selection histogram must match the number of distances: {len(selection_hist)} != {len(distances)}"
+        )
+    if not (num_selections := np.sum(selection_hist)):
+        return 1
+    normalised_distances = distances / np.min(distances)
+    return (
+        np.sum(
+            selection_hist
+            * np.exp(-(selection_hist / num_selections) / normalised_distances)
+        )
+        / num_selections
+    )
--- tests/util/test_embeddings.py
+++ tests/util/test_embeddings.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import numpy as np
+import pytest
 
-from cryoemservices.util.embeddings import distance_matrix
+from cryoemservices.util.embeddings import distance_exp_score, distance_matrix
 
 
 def test_distance_matrix_2d():
@@ -30,3 +31,24 @@ def test_distance_matrix_3d():
     assert dm.shape == (4, 4)
     assert dm[0][0] == 0
     assert dm[0][2] == dm[2][0]
+
+
+def test_distance_exp_score_wrong_dimensions():
+    with pytest.raises(ValueError):
+        selections = np.array([0, 0, 0, 0, 0])
+        distances = np.array([1, 2, 3, 4, 5])
+        distance_exp_score(selections, distances)
+
+
+def test_distance_exp_score_no_selections():
+    selections = np.array([0, 0, 0, 0, 0])
+    distances = np.array([1, 2, 3, 4, 5])
+    score = distance_exp_score(selections, distances)
+    assert score == 1
+
+
+def test_distance_exp_score_with_selections():
+    selections = np.array([0, 1, 0, 4, 0])
+    distances = np.array([1, 2, 3, 4, 5])
+    score = distance_exp_score(selections, distances)
+    assert score == (np.exp(-2 / 5) + np.exp(-16 / 5)) / 5
