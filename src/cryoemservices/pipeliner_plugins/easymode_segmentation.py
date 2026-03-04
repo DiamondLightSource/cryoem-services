b7b2301ef5e5656a1f295d4ba4111e94e95c2824
yxd92326 <stephen.riggs@diamond.ac.uk>
2026-03-04 14:47:41 +0000

    Shuffle things around

--- src/cryoemservices/pipeliner_plugins/easymode_segmentation.py
+++ src/cryoemservices/pipeliner_plugins/easymode_segmentation.py
@@ -6,56 +6,64 @@ import psutil
 from scipy.ndimage import zoom
 
 
+def segment_loop(xi, yi, zi, volume, model, tile_size, overlap, logger):
+    (pz, py, px) = tile_size
+    (oz, oy, ox) = overlap
+    d, h, w = volume.shape  # 672, 1440, 2016
+    sz, sy, sx = pz - 2 * oz, py - 2 * oy, px - 2 * ox  # non-overlapped patch size, 160
+
+    z_start = zi * sz - oz
+    y_start = yi * sy - oy
+    x_start = xi * sx - ox
+    vz0 = max(0, z_start)
+    vy0 = max(0, y_start)
+    vx0 = max(0, x_start)
+    vz1 = min(d, z_start + pz)
+    vy1 = min(h, y_start + py)
+    vx1 = min(w, x_start + px)
+    tile = np.zeros((pz, py, px), dtype=volume.dtype)
+    tile[
+        vz0 - z_start : vz1 - z_start,
+        vy0 - y_start : vy1 - y_start,
+        vx0 - x_start : vx1 - x_start,
+    ] = volume[vz0:vz1, vy0:vy1, vx0:vx1]
+    tile = np.expand_dims(np.array([tile]), axis=-1)
+    segmented_tile = model.predict(tile, verbose=0, batch_size=1).squeeze(-1)
+    logger.info(
+        f"Segmented {xi} {yi} {zi} at {psutil.Process(os.getpid()).memory_info().rss / 1024**2}"
+    )
+    z_end = min(zi * sz + sz, d)
+    y_end = min(yi * sy + sy, h)
+    x_end = min(xi * sx + sx, w)
+    az = z_end - zi * sz
+    ay = y_end - yi * sy
+    ax = x_end - xi * sx
+    return (
+        segmented_tile[0, oz : oz + sz, oy : oy + sy, ox : ox + sx][:az, :ay, :ax] * 127
+    ).astype(np.int8)
+
+
 def _segment_tomogram_instance(volume, model, tile_size, overlap, logger):
     (pz, py, px) = tile_size
     (oz, oy, ox) = overlap
-    d, h, w = volume.shape
-    sz, sy, sx = pz - 2 * oz, py - 2 * oy, px - 2 * ox
-    z_boxes = max(1, (d + sz - 1) // sz)
-    y_boxes = max(1, (h + sy - 1) // sy)
-    x_boxes = max(1, (w + sx - 1) // sx)
-    out = np.zeros((d, h, w), dtype=np.float32)
-    wgt = np.zeros((d, h, w), dtype=np.float32)
+    d, h, w = volume.shape  # 672, 1440, 2016
+    sz, sy, sx = pz - 2 * oz, py - 2 * oy, px - 2 * ox  # non-overlapped patch size, 160
+    z_boxes = max(1, (d + sz - 1) // sz)  # 5
+    y_boxes = max(1, (h + sy - 1) // sy)  # 9
+    x_boxes = max(1, (w + sx - 1) // sx)  # 13
+    logger.info(f"{volume.shape}, {z_boxes}, {y_boxes}, {x_boxes}")
+    out = np.ones((d, h, w), dtype=np.int8) * 0
     for zi in range(z_boxes):
         for yi in range(y_boxes):
             for xi in range(x_boxes):
-                logger.info(f"Starting {xi} {yi} {zi}")
-                z_start = zi * sz - oz
-                y_start = yi * sy - oy
-                x_start = xi * sx - ox
-                vz0 = max(0, z_start)
-                vy0 = max(0, y_start)
-                vx0 = max(0, x_start)
-                vz1 = min(d, z_start + pz)
-                vy1 = min(h, y_start + py)
-                vx1 = min(w, x_start + px)
-                extracted = volume[vz0:vz1, vy0:vy1, vx0:vx1]
-                tile = np.zeros((pz, py, px), dtype=volume.dtype)
-                tz0 = vz0 - z_start
-                ty0 = vy0 - y_start
-                tx0 = vx0 - x_start
-                tile[
-                    tz0 : tz0 + extracted.shape[0],
-                    ty0 : ty0 + extracted.shape[1],
-                    tx0 : tx0 + extracted.shape[2],
-                ] = extracted
-                logger.info(f"{extracted.shape} {tile.shape}")
                 z_pos, y_pos, x_pos = zi * sz, yi * sy, xi * sx
-                tile = np.expand_dims(np.array([tile]), axis=-1)
-                segmented_tile = model.predict(tile, verbose=0, batch_size=1)
-                logger.info(
-                    f"Segmented at {psutil.Process(os.getpid()).memory_info().rss / 1024**2}"
-                )
-                segmented_tile = segmented_tile.squeeze(-1)
-                center = segmented_tile[0, oz : oz + sz, oy : oy + sy, ox : ox + sx]
                 z_end = min(z_pos + sz, d)
                 y_end = min(y_pos + sy, h)
                 x_end = min(x_pos + sx, w)
-                az, ay, ax = z_end - z_pos, y_end - y_pos, x_end - x_pos
-                out[z_pos:z_end, y_pos:y_end, x_pos:x_end] += center[:az, :ay, :ax]
-                wgt[z_pos:z_end, y_pos:y_end, x_pos:x_end] += 1
-    wgt[wgt == 0] = 1
-    return out / wgt
+                out[z_pos:z_end, y_pos:y_end, x_pos:x_end] = segment_loop(
+                    xi, yi, zi, volume, model, tile_size, overlap, logger
+                )
+    return out
 
 
 def _pad_volume(volume, min_pad=16, div=32):
@@ -91,11 +99,15 @@ def segment_tomogram(
         0 if tile_size[1] == ok else 48,
         0 if tile_size[2] == ol else 48,
     )
-    logger.info("Doing instance")
+    logger.info(
+        f"Doing instance {psutil.Process(os.getpid()).memory_info().rss / 1024**2}"
+    )
     segmented_volume = _segment_tomogram_instance(
         volume, model, tile_size, overlap, logger
     )
-    logger.info("Done instance")
+    logger.info(
+        f"Done instance {psutil.Process(os.getpid()).memory_info().rss / 1024**2}"
+    )
     (j0, j1), (k0, k1), (l0, l1) = padding
     segmented_volume = segmented_volume[
         j0 : segmented_volume.shape[0] - j1,
