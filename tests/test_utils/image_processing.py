add2e3ffc0f471ab5bf5f6a72bfecb2501bf0024
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-03-11 09:19:38 +0000

    Optimised test util for creating grayscale images with features, and updated documentation

--- tests/test_utils/image_processing.py
+++ tests/test_utils/image_processing.py
@@ -5,14 +5,34 @@ import numpy as np
 
 def gaussian_2d(
     shape: tuple[int, int],
-    amplitude: float,
+    amplitude: int | float,
     centre: tuple[int, int],
     sigma: tuple[float, float],
     theta: float,
-    offset: float,
+    offset: int | float,
 ):
     """
-    Helper function to create Gaussian peaks
+    Helper function to create Gaussian peaks.
+
+    Parameters
+    ----------
+    shape: tuple[int, int]
+        The shape of the image frame to place the 2D Gaussian in
+
+    amplitude: int | float
+        Amplitude of the Gaussian
+
+    centre: tuple[int, int]
+        The x- and y-coordinates around which the Gaussian is based
+
+    sigma: tuple[float, float]
+        The length of the major and minor axes
+
+    theta: float
+        The angle of the Gaussian, in degrees
+
+    offset: int | float
+        The intensity offset of the Gaussian
     """
     x0, y0 = centre
     sig_x, sig_y = sigma
@@ -46,26 +66,47 @@ def create_grayscale_image(
     intensity_offset_per_frame: int,
 ):
     """
-    Creates a grayscale image with peaks that are offset from frame-to-frame
+    Creates a grayscale image with features (Gaussian peaks) that are offset from
+    frame-to-frame.
+
+    Parameters
+    ----------
+    shape: tuple[int, int]
+        The shape of the image frame
+
+    num_frames: int
+        The number of frames in the image stack
+
+    dtype: str
+        The dtype of the image
+
+    peaks: list[dict]
+        A list of kwargs that will be passed to the 2D Gaussian creation function
+        to populate the image. The dictionaries should contain the following keys:
+            - shape: tuple[int, int]
+            - amplitude: int
+            - centre:
+            - sigma
+            - theta
+            - offset
+
     """
 
     x_shift, y_shift = peak_shift_per_frame
     c_off = intensity_offset_per_frame
-    if num_frames == 1:
-        arr = np.zeros(shape, dtype=dtype)
+
+    arr = np.zeros((num_frames, *shape), dtype=dtype)
+    for f in range(num_frames):
         for peak in peaks:
-            arr += gaussian_2d(**peak).astype(dtype)
-    else:
-        arr = np.zeros((num_frames, *shape), dtype=dtype)
-        for f in range(num_frames):
-            for peak in peaks:
-                # Adjust the peak offset per frame
-                centre: tuple[int, int] = peak["centre"]
-                x, y = centre
-                peak["centre"] = (x + x_shift, y + y_shift)
-                peak["offset"] += c_off
-                arr[f] += gaussian_2d(**peak).astype(dtype)
+            arr[f] += gaussian_2d(**peak).astype(dtype)
+            # Adjust the peak offset per frame after updating the current one
+            centre: tuple[int, int] = peak["centre"]
+            x, y = centre
+            peak["centre"] = (x + x_shift, y + y_shift)
+            peak["offset"] += c_off
 
     arr = arr.astype(dtype)
+    if num_frames == 1:
+        arr = arr[0]
 
     return arr
