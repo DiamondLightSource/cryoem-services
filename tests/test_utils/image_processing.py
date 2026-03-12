513088e42bbf85d922f3d70de315e29eb74ced13
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-03-12 04:33:05 +0000

    Added utility function to create grayscale images containing hole-like features for tests

--- tests/test_utils/image_processing.py
+++ tests/test_utils/image_processing.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import cv2
 import numpy as np
 
 
@@ -85,10 +86,10 @@ def create_grayscale_image_with_gaussian(
         to populate the image. The dictionaries should contain the following keys:
             - shape: tuple[int, int]
             - amplitude: int
-            - centre:
-            - sigma
-            - theta
-            - offset
+            - centre: tuple[int, int]
+            - sigma: tuple[float, float]
+            - theta: float
+            - offset: int | float
 
     """
 
@@ -109,4 +110,81 @@ def create_grayscale_image_with_gaussian(
     if num_frames == 1:
         arr = arr[0]
 
+    return arr
+
+
+def create_grayscale_image_with_holes(
+    shape: tuple[int, int],
+    num_frames: int,
+    layer_intensity: int,
+    noise_sigma: int,
+    hole_intensity: int,
+    hole_list: list[dict[str, Any]],
+    offset_per_frame: tuple[int, int],
+):
+    """
+    Creates a textured test image with hole-like features in it to test the
+    ORB-based image registration function with.
+
+    Parameters
+    ----------
+    shape: tuple[int, int]
+        The shape of the image frame
+
+    num_frames: int
+        The number of frames in the image stack
+
+    layer_intensity: int
+        The pixel value to use for the simulated layer in the image
+
+    noise_sigma: int
+        The amount of Gaussian noise to introduce into the image
+
+    hole_intensity: int
+        The pixel value to use to represent the hole
+
+    hole_list: list[dict[str, Any]]
+        A list of dictionaries used to generate the holes needed for this
+        test image. The needed parameters are:
+        - center: tuple[int, int] |
+            The x and y coordinates of the hole
+        - axes: tuple[int, int] |
+            The lengths of the major and minor axes
+        - angle: float |
+            The angle of the hole, in degrees
+
+    offset_per_frame: tuple[int, int]
+        The x and y offset to shift the image by each frame
+    """
+
+    # Create a noisy array
+    arr = np.ones((num_frames, *shape), dtype=np.float32) * layer_intensity
+    arr += np.random.normal(0, noise_sigma, (num_frames, *shape))
+
+    # Iterate and create holes
+    x_offset, y_offset = offset_per_frame
+    for f in range(num_frames):
+        for hole_info in hole_list:
+            center: tuple[int, int] = hole_info["center"]
+            cx, cy = center
+            axes: tuple[int, int] = hole_info["axes"]
+            ax, ay = axes
+
+            arr[f] = cv2.ellipse(
+                arr[f],
+                center=(cx + (x_offset * f), cy + (y_offset * f)),
+                axes=(ax, ay),
+                angle=hole_info["angle"],
+                startAngle=0,
+                endAngle=360,
+                color=hole_intensity,
+                thickness=-1,
+            )
+        # Apply slight Gaussian blurring
+        arr[f] = cv2.GaussianBlur(arr[f], (0, 0), 1.0)
+
+    np.clip(arr, 0, 255, out=arr)
+    arr = arr.astype(np.uint8)
+    if num_frames == 1:
+        arr = arr[0]
     return arr
