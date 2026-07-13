d7764fc38feb52cea244ee591136b811d1148b41
yxd92326 <stephen.riggs@diamond.ac.uk>
2026-07-13 11:37:15 +0100

    Missed one

--- src/cryoemservices/services/tomo_align_aretomo.py
+++ src/cryoemservices/services/tomo_align_aretomo.py
@@ -860,19 +860,12 @@ class AreTomoAlign(CommonService):
             # Take file name for Relion-type projects, or folder name for SXT-style
             stack_name = (
                 Path(tomo_params.stack_file).name
-                if np.array(
-                    [
-                        re.match("job[0-9]+", p)
-                        for p in Path(tomo_params.stack_file).parts
-                    ]
-                ).any()
+                if re.match("job[0-9]+", tomo_params.stack_file)
                 else f"{Path(tomo_params.stack_file).parent.parent}_stack.mrc"
             )
             tomo_name = (
                 aretomo_output_path.name
-                if np.array(
-                    [re.match("job[0-9]+", p) for p in aretomo_output_path.parts]
-                ).any()
+                if re.match("job[0-9]+", str(aretomo_output_path))
                 else f"{aretomo_output_path.parent.parent}_volume.mrc"
             )
             shutil.copy(Path(tomo_params.stack_file), project_dir.parent / stack_name)
