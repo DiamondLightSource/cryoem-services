bc24ac5edc3d9071610d8d8383a7ad8dabbf4652
yxd92326 <stephen.riggs@diamond.ac.uk>
2026-09-22 16:40:23 +0100

    Do move before checking outcome

--- src/cryoemservices/services/tomo_align_imod.py
+++ src/cryoemservices/services/tomo_align_imod.py
@@ -171,33 +171,6 @@ class ImodTomoAlign(CommonService):
             ],
             capture_output=True,
         )
-        if imod_result.returncode:
-            self.log.error(
-                f"batchruntomo failed with exitcode {imod_result.returncode}"
-            )
-            # Update failure processing status, then try again
-            rw.send_to("failure", {})
-            self._reject_message(header, rw.transport)
-            return
-        elif not (uuid_dir / imod_output_path.name).is_file():
-            self.log.error(
-                f"batchruntomo did not produce the output file {imod_output_path}\n"
-                + imod_result.stdout.decode("utf8", "replace")
-                + imod_result.stderr.decode("utf8", "replace")
-            )
-            # Update failure processing status, but do not try again as return was 0
-            rw.send_to("failure", {})
-            self._reject_message(header, rw.transport, requeue=False)
-            return
-
-        # Generate shift plot for ispyb
-        plot_file = imod_output_path.stem + "_xy_shift_plot.json"
-        plot_path = uuid_dir / plot_file
-        xf_file = self.extract_from_xf(uuid_stack, plot_path)
-        if not xf_file:
-            self.log.error("Failed to read alignment file")
-            self._reject_message(header, transport=rw.transport)
-            return
 
         # Move everything out of the uuid folder
         def iterative_move(dir_to_move):
@@ -214,6 +187,35 @@ class ImodTomoAlign(CommonService):
             dir_to_move.rmdir()
 
         iterative_move(uuid_dir)
+
+        # Check the process outcome
+        if imod_result.returncode:
+            self.log.error(
+                f"batchruntomo failed with exitcode {imod_result.returncode}"
+            )
+            # Update failure processing status, then try again
+            rw.send_to("failure", {})
+            self._reject_message(header, rw.transport)
+            return
+        elif not imod_output_path.is_file():
+            self.log.error(
+                f"batchruntomo did not produce the output file {imod_output_path}\n"
+                + imod_result.stdout.decode("utf8", "replace")
+                + imod_result.stderr.decode("utf8", "replace")
+            )
+            # Update failure processing status, but do not try again as return was 0
+            rw.send_to("failure", {})
+            self._reject_message(header, rw.transport, requeue=False)
+            return
+
+        # Generate shift plot for ispyb
+        plot_file = imod_output_path.stem + "_xy_shift_plot.json"
+        plot_path = imod_output_path.parent / plot_file
+        xf_file = self.extract_from_xf(Path(tomo_params.stack_file), plot_path)
+        if not xf_file:
+            self.log.error("Failed to read alignment file")
+            self._reject_message(header, transport=rw.transport)
+            return
 
         # Insert tomogram into ispyb
         side_projection = (
@@ -258,7 +260,7 @@ class ImodTomoAlign(CommonService):
             {
                 "image_command": "tilt_series_alignment",
                 "file": tomo_params.stack_file,
-                "xf_file": str(uuid_dir.parent / xf_file.name),
+                "xf_file": str(xf_file),
                 "pixel_size": tomo_params.pixel_size,
                 "projection": side_projection,
             },
